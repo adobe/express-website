0645dbf5b7256abea9bf6423ee1736f43f412f17
David Nuescheler <david@nuescheler.ch>
2021-03-16 13:26:55 -0700

    feat(page-list): initial page-list block

---
+++ express/blocks/page-list/page-list.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* global window fetch */
+/* eslint-disable import/named, import/extensions */
+
+import {
+  createTag,
+  readBlockConfig,
+} from '../../scripts/scripts.js';
+
+function addPages(index, filter, $block) {
+  index.forEach((page) => {
+    if (page.path.includes(filter)) {
+      let { path } = page;
+      if (!path.startsWith(' ')) path = path.substr(1);
+      path = path.replace('.html', '');
+      const $card = createTag('div', { class: 'card' });
+      $card.innerHTML = `<div class="card-image">
+            <img loading="lazy" src="${page.image.replace('width=2000', 'width=750')}">
+          </div>
+          <div class="card-body">
+            <h3>${page.title}</h3>
+          </div>`;
+      $card.addEventListener('click', () => {
+        window.location.href = path;
+      });
+      $block.appendChild($card);
+    }
+  });
+}
+
+async function fetchIndex() {
+  /*
+  const locale = getLocale();
+  const indexURL = locale === 'en' ? '/express/query-index.json' : `/${locale}/query-index.json`;
+  */
+
+  const indexURL = '/drafts/uncled/query-index.json';
+  try {
+    const resp = await fetch(indexURL);
+    const json = await resp.json();
+    // eslint-disable-next-line no-console
+    console.log(`${indexURL}: ${json.data.length}`);
+    return (json.data);
+  } catch (e) {
+    // something went wrong
+    return ([]);
+  }
+}
+
+async function decoratePageList($block) {
+  const config = readBlockConfig($block);
+  $block.innerHTML = '';
+  const index = await fetchIndex();
+  addPages(index, config.filter, $block);
+}
+
+export default function decorate($block) {
+  decoratePageList($block);
+}
