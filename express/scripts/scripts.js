eae54a78d85e9e55939d750cf181cfb31573ebe9
David Nuescheler <david@nuescheler.ch>
2021-03-23 13:42:14 -0700

    fix: design

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -180,7 +180,7 @@ function decorateBlocks() {
     if ($section) {
       $section.classList.add(`${blockName}-container`.replaceAll('--', '-'));
     }
-    const blocksWithOptions = ['checker-board', 'template-list', 'steps', 'cards', 'quotes', 'page-list'];
+    const blocksWithOptions = ['checker-board', 'template-list', 'steps', 'cards', 'quotes', 'page-list', 'columns'];
     blocksWithOptions.forEach((b) => {
       if (blockName.startsWith(`${b}-`)) {
         const options = blockName.substring(b.length + 1).split('-').filter((opt) => !!opt);
