55912173d9adf314f0a760bdafd176dd2862dae3
David Nuescheler <david@nuescheler.ch>
2021-04-01 16:41:05 -0700

    chore: adding chevron icon

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -47,7 +47,8 @@ export function getIcon(icon, alt = icon) {
   const symbols = ['adobe', 'adobe-red', 'facebook', 'instagram', 'pinterest',
     'linkedin', 'twitter', 'youtube', 'discord', 'behance', 'creative-cloud',
     'hamburger', 'adchoices', 'play', 'not-found', 'snapchat', 'learn', 'magicwand',
-    'upload', 'resize', 'download', 'creativecloud', 'shapes', 'users', 'color', 'stickers', 'landscape', 'globe'];
+    'upload', 'resize', 'download', 'creativecloud', 'shapes', 'users', 'color', 'stickers', 'landscape', 
+    'globe', 'chevron'];
   if (symbols.includes(icon)) {
     return `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-${icon}">
       <use href="/express/icons.svg#${icon}"></use>
