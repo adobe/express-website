e77f28c20640eeb49397e1589fa153ef3fafc59d
David Nuescheler <david@nuescheler.ch>
2021-04-02 13:03:54 -0700

    feat(gnav): placeholder customizable

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -300,6 +300,45 @@ export function addBlockClasses($block, classNames) {
 //   });
 // }
 
+function getGnavPlaceholder(nav) {
+  let html = `<div id="feds-header">
+    </div>
+    <div id="header-placeholder" class="placeholder">
+    <div class="mobile">
+      <div class="hamburger"></div>
+      <div class="logo"><img src="/express/gnav-placeholder/adobe-logo.svg"></div>
+      <div class="signin">${nav.signIn}</div>
+    </div>
+    <div class="desktop">
+      <div class="top">
+        <div class="left">
+          <div class="logo"><img src="/express/gnav-placeholder/adobe-logo.svg"><span class="adobe">Adobe</span></div>
+          <div class="section">`;
+
+  nav.top.forEach((e) => {
+    const selected = e.selected ? 'selected' : '';
+    if (e.type === 'nodrop') {
+      html += `<span class="${selected}">${e.text}</span>`;
+    } else if (e.type === 'button') {
+      html += `<span><a href="#" class="button primary">${e.text}</a></span>`;
+    } else {
+      html += `<span class="drop ${selected}">${e.text}</span>`;
+    }
+  });
+
+  html += `</div>
+        </div>
+        <div class="right">
+          <div class="search"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" focusable="false">
+          <path d="M14 2A8 8 0 0 0 7.4 14.5L2.4 19.4a1.5 1.5 0 0 0 2.1 2.1L9.5 16.6A8 8 0 1 0 14 2Zm0 14.1A6.1 6.1 0 1 1 20.1 10 6.1 6.1 0 0 1 14 16.1Z"></path>
+      </svg></div>
+          <div class="signing">${nav.signIn}</div>
+        </div>
+      </div>
+    </div>`;
+  return (html);
+}
+
 function decorateHeaderAndFooter() {
   const $header = document.querySelector('header');
 
@@ -307,58 +346,48 @@ function decorateHeaderAndFooter() {
   const locale = getLocale(window.location);
 
   if (locale === 'us') {
-    $header.innerHTML = `
-    <div id="feds-header">
-    </div>
-    <div id="header-placeholder" class="placeholder">
-    <div class="mobile">
-      <div class="hamburger"></div>
-      <div class="logo"><img src="/express/gnav-placeholder/adobe-logo.svg"></div>
-      <div class="signin">Sign In</div>
-    </div>
-    <div class="desktop">
-      <div class="top">
-        <div class="left">
-          <div class="logo"><img src="/express/gnav-placeholder/adobe-logo.svg"><span class="adobe">Adobe</span></div>
-          <div class="section">
-            <span>Adobe Spark</span>
-            <span class="drop">Features</span>
-            <span class="drop">Create</span>
-            <span class="drop">Discover</span>
-            <span class="drop">Learn</span>
-            <span>Compare plans</span>
-            <span><a href="#" class="button primary">Start now</a></span>
-          </div>
-        </div>
-        <div class="right">
-          <div class="search"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" focusable="false">
-          <path d="M14 2A8 8 0 0 0 7.4 14.5L2.4 19.4a1.5 1.5 0 0 0 2.1 2.1L9.5 16.6A8 8 0 1 0 14 2Zm0 14.1A6.1 6.1 0 1 1 20.1 10 6.1 6.1 0 0 1 14 16.1Z"></path>
-      </svg></div>
-          <div class="signing">Sign In</div>
-        </div>
-      </div>
-    </div>`;
+    const nav = {
+      signIn: 'Sign In',
+      top: [
+        {
+          text: 'Adobe Spark',
+          type: 'nodrop',
+        },
+        {
+          text: 'Features',
+        },
+        {
+          text: 'Create',
+        },
+        {
+          text: 'Discover',
+        },
+        {
+          text: 'Learn',
+        },
+        {
+          text: 'Compare Plans',
+          type: 'nodrop',
+        },
+        {
+          text: 'Start now',
+          type: 'button',
+        },
+      ],
+    };
+    if (window.location.pathname === '/express/') nav.top[0].selected = true;
+    if (window.location.pathname.startsWith('/express/feature')) nav.top[1].selected = true;
+    if (window.location.pathname.startsWith('/express/create')) nav.top[2].selected = true;
+    if (window.location.pathname.startsWith('/express/discover')) nav.top[3].selected = true;
+    if (window.location.pathname.startsWith('/express/learn')) nav.top[4].selected = true;
+    if (window.location.pathname.startsWith('/express/pricing')) nav.top[5].selected = true;
+    const html = getGnavPlaceholder(nav);
+    $header.innerHTML = html;
   } else {
-    $header.innerHTML = `
-    <div id="feds-header">
-    </div>
-    <div id="header-placeholder" class="placeholder">
-    <div class="mobile">
-      <div class="hamburger"></div>
-      <div class="logo"><img src="/express/gnav-placeholder/adobe-logo.svg"></div>
-      <div class="signin">Sign In</div>
-    </div>
-    <div class="desktop">
-      <div class="top">
-        <div class="left">
-          <div class="logo"><img src="/express/gnav-placeholder/adobe-logo.svg"><span class="adobe">Adobe</span></div>
-          <div class="section">
-          </div>
-        </div>
-        <div class="right">
-        </div>
-      </div>
-    </div>`;
+    $header.innerHTML = getGnavPlaceholder({
+      signIn: 'Sign In',
+      top: [],
+    });
   }
 
   document.querySelector('footer').innerHTML = `
@@ -543,15 +572,18 @@ export function readBlockConfig($block) {
 async function loadFont(name, url, weight) {
   const font = new FontFace(name, url, { weight });
   const fontLoaded = await font.load();
-  document.fonts.add(fontLoaded);
   return (fontLoaded);
 }
 
 async function loadFonts() {
   try {
-    await loadFont('adobe-clean', 'url("https://use.typekit.net/af/b0c5f5/00000000000000003b9b3f85/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3")', 400);
-    await loadFont('adobe-clean', 'url("https://use.typekit.net/af/ad2a79/00000000000000003b9b3f8c/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3")', 900);
-    await loadFont('adobe-clean', 'url("https://use.typekit.net/af/97fbd1/00000000000000003b9b3f88/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3")', 700);
+    /* todo promise.All */
+    const f900 = await loadFont('adobe-clean', 'url("https://use.typekit.net/af/b0c5f5/00000000000000003b9b3f85/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3")', 400);
+    const f400 = await loadFont('adobe-clean', 'url("https://use.typekit.net/af/ad2a79/00000000000000003b9b3f8c/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3")', 900);
+    const f700 = await loadFont('adobe-clean', 'url("https://use.typekit.net/af/97fbd1/00000000000000003b9b3f88/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3")', 700);
+    document.fonts.add(f900);
+    document.fonts.add(f400);
+    document.fonts.add(f700);
   } catch (err) {
     /* something went wrong */
     console.log(err);
