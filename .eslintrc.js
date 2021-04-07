b0ed58d39802aa8b9d05be9c379e4afc1e369f3f
William Ste-Marie <williambsm@hotmail.fr>
2021-04-07 15:24:04 -0400

    fix: eslint to LF

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
     'import/extensions': ['error', {
       js: 'always',
     }],
