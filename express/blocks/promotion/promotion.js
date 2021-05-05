c25d2ac33a8004a73840a81869603d779c8cce07
Alexandre Capt <acapt@adobe.com>
2021-05-05 10:57:51 +0200

    feat(promo): real path and error handling

--- express/blocks/promotion/promotion.js
+++ express/blocks/promotion/promotion.js
@@ -19,55 +19,62 @@ import {
   fixIcons,
 } from '../../scripts/scripts.js';
 
-const PROMOTION_FOLDER = 'drafts/alex';
+const PROMOTION_FOLDER = 'express/promotions';
 
 async function fetchPromotion(name) {
   const locale = getLocale(window.location);
-  const promoURL = `${locale === 'us' ? '' : locale}/${PROMOTION_FOLDER}/${name}.plain.html`;
+  const promoURL = `${locale === 'us' ? '' : `/${locale}`}/${PROMOTION_FOLDER}/${name}.plain.html`;
   const resp = await fetch(promoURL);
-  const html = await resp.text();
-  return html;
+  if (resp.ok) {
+    const html = await resp.text();
+    return html;
+  }
+  return null;
 }
 
 export default async function decorate($block) {
   const name = $block.textContent;
   const html = await fetchPromotion(name);
 
-  const div = createTag('div');
-  div.innerHTML = html;
+  if (html) {
+    const div = createTag('div');
+    div.innerHTML = html;
 
-  normalizeHeadings(div, ['h2', 'h3']);
+    normalizeHeadings(div, ['h2', 'h3']);
 
-  const h2 = div.querySelector('h2');
+    const h2 = div.querySelector('h2');
 
-  const containerDiv = createTag('div', { class: 'promotion-wrapper' });
+    const containerDiv = createTag('div', { class: 'promotion-wrapper' });
 
-  const heroPicture = div.querySelector('picture');
-  if (heroPicture) {
-    const p = heroPicture.parentNode;
-    const heroDiv = createTag('div', { class: 'promotion-hero' });
-    heroDiv.append(heroPicture);
+    const heroPicture = div.querySelector('picture');
+    if (heroPicture) {
+      const p = heroPicture.parentNode;
+      const heroDiv = createTag('div', { class: 'promotion-hero' });
+      heroDiv.append(heroPicture);
 
-    containerDiv.append(heroDiv);
-    p.remove();
+      containerDiv.append(heroDiv);
+      p.remove();
+    }
+
+    const contentDiv = createTag('div', { class: 'promotion-content' });
+    contentDiv.append(div.firstChild);
+
+    containerDiv.append(contentDiv);
+
+    $block.innerHTML = '';
+    $block.append(h2);
+    $block.append(containerDiv);
+
+    decorateButtons($block);
+    fixIcons($block);
+
+    // apply primary light button styles
+    $block.querySelectorAll('.button.accent').forEach((b) => {
+      b.classList.remove('accent');
+      b.classList.add('primary');
+      b.classList.add('light');
+    });
+  } else {
+    $block.innerHTML = '';
   }
-
-  const contentDiv = createTag('div', { class: 'promotion-content' });
-  contentDiv.append(div.firstChild);
-
-  containerDiv.append(contentDiv);
-
-  $block.innerHTML = '';
-  $block.append(h2);
-  $block.append(containerDiv);
-
-  decorateButtons($block);
-  fixIcons($block);
-
-  // apply primary light button styles
-  $block.querySelectorAll('.button.accent').forEach((b) => {
-    b.classList.remove('accent');
-    b.classList.add('primary');
-    b.classList.add('light');
-  });
 }
