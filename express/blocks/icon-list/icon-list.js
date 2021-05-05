7bc80ac0a6d7c50d06b26b7e086ff0da29be52c0
David Nuescheler <david@nuescheler.ch>
2021-05-04 18:27:28 -0600

    feat: inital icon list refactor

--- express/blocks/icon-list/icon-list.js
+++ express/blocks/icon-list/icon-list.js
@@ -15,16 +15,50 @@ import {
   toClassName,
   getIcon,
   addBlockClasses,
+  createTag,
 } from '../../scripts/scripts.js';
 
 export default function decorate($block) {
-  addBlockClasses($block, ['icon-list-image', 'icon-list-description']);
-  $block.querySelectorAll(':scope>div').forEach(($row) => {
-    if ($row.children && $row.children[1]) {
-      const iconName = toClassName($row.children[0].textContent);
-      if (iconName && !iconName.startsWith('-')) {
-        $row.children[0].innerHTML = iconName ? getIcon(iconName) : '';
+  let numCols = 0;
+  const $rows = [...$block.children];
+  if ($rows[0]) {
+    numCols = $rows[0].children.length;
+  }
+  if (numCols === 2) {
+    /* legacy icon list */
+    addBlockClasses($block, ['icon-list-image', 'icon-list-description']);
+    $block.querySelectorAll(':scope>div').forEach(($row) => {
+      if ($row.children && $row.children[1]) {
+        const iconName = toClassName($row.children[0].textContent);
+        if (iconName && !iconName.startsWith('-')) {
+          $row.children[0].innerHTML = iconName ? getIcon(iconName) : '';
+        }
       }
-    }
-  });
+    });
+  }
+
+  if (numCols === 4) {
+    const $cols = ['left', 'right'].map(() => createTag('div', { class: 'icon-list-column' }));
+    $rows.forEach(($row, i) => {
+      $cols.forEach(($col) => $col.append(createTag('div')));
+      const $cells = [...$row.children];
+      $cells.forEach(($cell, j) => {
+        $cols[Math.floor(j / 2)].children[i].append($cell);
+        if (j % 2) {
+          if ($cell.querySelector('h3')) {
+            $cell.parentNode.classList.add('icon-list-heading');
+          } else {
+            $cell.parentNode.classList.add('icon-list-regular');
+          }
+        }
+      });
+      $row.remove();
+    });
+    $cols.forEach(($col) => {
+      addBlockClasses($col, ['icon-list-image', 'icon-list-description']);
+      $block.append($col);
+    });
+    $block.classList.add('two-column');
+    $block.closest('div.section-wrapper').classList.add('icon-list-two-column-container');
+  }
 }
