710296c4040274bc4b1f56ebfbe66394d348ecca
David Nuescheler <david@nuescheler.ch>
2021-05-05 16:28:29 -0600

    chore: remove old cc icon

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -57,7 +57,7 @@ function getMeta(name) {
 
 export function getIcon(icon, alt = icon) {
   const symbols = ['adobe', 'adobe-red', 'facebook', 'instagram', 'pinterest',
-    'linkedin', 'twitter', 'youtube', 'discord', 'behance', 'creative-cloud',
+    'linkedin', 'twitter', 'youtube', 'discord', 'behance',
     'hamburger', 'adchoices', 'play', 'not-found', 'snapchat', 'learn', 'magicwand',
     'upload', 'resize', 'download', 'creativecloud', 'shapes', 'users', 'color', 'stickers', 'landscape',
     'globe', 'chevron'];
