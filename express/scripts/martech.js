bd6292195b24ecb27ed66d92293acaae425e7927
William Ste-Marie <williambsm@hotmail.fr>
2021-08-03 15:53:12 -0400

    152 to 155 analytics

--- express/scripts/martech.js
+++ express/scripts/martech.js
@@ -188,10 +188,29 @@ loadScript('https://www.adobe.com/marketingtech/main.min.js', () => {
   if (pathname.includes('/feature/image/resize')) {
     digitalData._set('spark.eventData.contextualData1', 'quickActionType:imageResize');
     digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
-  }
-  // image remove background quick action
-  if (pathname.includes('/feature/image/transparent-background')) {
+  } else if (pathname.includes('/feature/image/transparent-background')) {
     digitalData._set('spark.eventData.contextualData1', 'quickActionType:removeBackground');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/change-speed')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:changeVideoSpeed');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/merge')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:mergeVideo');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/convert/mp4-to-gif')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:convertToGIF');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/convert/mp4')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:convertToMP4');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/convert/gif-to-mp4')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:convertToMP4');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/convert/mov-to-mp4')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:convertToMP4');
+    digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
+  } else if (pathname.endsWith('/feature/video/convert/wmv-to-mp4')) {
+    digitalData._set('spark.eventData.contextualData1', 'quickActionType:convertToMP4');
     digitalData._set('spark.eventData.contextualData2', 'actionLocation:seo');
   }
 
