f61475668d5dd82c1aa48037541686c5870139dd
rofe <github@rofe.com>
2021-08-09 14:25:40 +0200

    test: template-list with title

--- test/unit/blocks/blocks-test-list.js
+++ test/unit/blocks/blocks-test-list.js
@@ -52,6 +52,10 @@ export default [
     input: 'input/template-list.horizontal.doc.html',
     expected: 'expected/template-list.horizontal.block.html',
   }, {
+    name: 'Template List - title',
+    input: 'input/template-list.title.doc.html',
+    expected: 'expected/template-list.title.block.html',
+  }, {
     name: 'Columns - single row',
     input: 'input/columns.singlerow.doc.html',
     expected: 'expected/columns.singlerow.block.html',
