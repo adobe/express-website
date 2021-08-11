273fbf51223fd5f3d49286ae8e91fba1aed9be7c
Alexandre Capt <acapt@adobe.com>
2021-08-11 14:59:19 +0200

    feat: how to carousel

---
+++ express/blocks/how-to-steps-carousel/how-to-steps-carousel.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-disable import/named, import/extensions */
+
+import {
+  createTag,
+} from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  // move first image of container outside of div for styling
+  const picture = block.parentElement.querySelector('picture');
+  const parent = picture.parentElement;
+  block.parentElement.before(picture);
+  parent.remove();
+
+  const howto = block;
+  const rows = Array.from(howto.children);
+
+  const numbers = createTag('div', { class: 'tip-numbers' });
+  block.prepend(numbers);
+
+  rows.forEach((row, i) => {
+    row.classList.add('tip');
+    row.classList.add(`tip-${i + 1}`);
+
+    const cells = Array.from(row.children);
+
+    const h3 = createTag('h3');
+    h3.innerHTML = cells[0].textContent;
+    const p = createTag('p');
+    p.innerHTML = cells[1].textContent;
+    const text = createTag('div', { class: 'tip-text' });
+    text.append(h3);
+    text.append(p);
+
+    row.innerHTML = '';
+    row.append(text);
+
+    const number = createTag('div', { class: 'tip-number', tabindex: '0' });
+    number.innerHTML = `<span>${i + 1}</span>`;
+    number.addEventListener('click', (e) => {
+      block.querySelectorAll('.tip, .tip-number').forEach((item) => {
+        item.classList.remove('active');
+      });
+      let { target } = e;
+      if (e.target.nodeName.toLowerCase() === 'span') {
+        target = e.target.parentElement;
+      }
+      target.classList.add('active');
+      block.querySelector(`.tip-${i + 1}`).classList.add('active');
+    });
+    number.addEventListener('keyup', (e) => {
+      if (e.which === 13) {
+        e.preventDefault();
+        e.target.click();
+      }
+    });
+    numbers.append(number);
+
+    if (i === 0) {
+      row.classList.add('active');
+      number.classList.add('active');
+    }
+  });
+}
--- test/unit/blocks/blocks-test-list.js
+++ test/unit/blocks/blocks-test-list.js
@@ -102,11 +102,11 @@ export default [
   }, {
     name: 'Inline Banner - inverted if dark container',
     input: 'input/inline-banner.dark.doc.html',
-    expected: 'expected/inline-banner.inverted.block.html',
+    expected: 'expected/inline-banner.dark.section.html',
   }, {
     name: 'Inline Banner - inverted if highlighted container',
     input: 'input/inline-banner.highlight.doc.html',
-    expected: 'expected/inline-banner.inverted.block.html',
+    expected: 'expected/inline-banner.inverted.section.html',
   }, {
     name: 'Commerce CTA',
     input: 'input/commerce-cta.doc.html',
@@ -119,5 +119,9 @@ export default [
     name: 'FAQ - basic',
     input: 'input/faq.doc.html',
     expected: 'expected/faq.block.html',
+  }, {
+    name: 'How To Steps Carousel',
+    input: 'input/how-to-steps-carousel.doc.html',
+    expected: 'expected/how-to-steps-carousel.section.html',
   },
 ];
