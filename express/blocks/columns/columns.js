b7d7e8fc88111b4d61c377c4b6ef8494387cd98a
Marquise Rosier <mrosier@adobe.com>
2021-09-02 02:04:42 -0700

    fix(columns.js): add missing parameter
    add missing parameter to applySizing

--- express/blocks/columns/columns.js
+++ express/blocks/columns/columns.js
@@ -167,7 +167,7 @@ export default function decorate($block) {
     7: 's',
   };
   const headings = document.querySelectorAll('main .columns h1, main .columns h2, main .columns h3, main .columns h4, main .columns h5, main .columns h6');
-  applySizing(headings);
+  applySizing(headings, sizes);
   const scaleCB = scaleHeadings.bind(headings, sizes);
   scaleCB();
   window.addEventListener('resize', scaleCB);
