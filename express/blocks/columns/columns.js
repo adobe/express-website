e3020df4ebeb45021594f9e034f675bd06c394c1
William Ste-Marie <williambsm@hotmail.fr>
2022-01-18 10:02:42 -0500

    fix(columns): prevent clicking youtube or vimeo link

--- express/blocks/columns/columns.js
+++ express/blocks/columns/columns.js
@@ -157,6 +157,10 @@ export default function decorate($block) {
         if (($a.href.includes('vimeo') || $a.href.includes('youtu'))
           && $row.parentElement.classList.contains('highlight')) {
           transformToVideoColumn($cell, $a);
+
+          $a.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
         }
         if ($a.textContent.startsWith('https://')) {
           if ($a.href.endsWith('.mp4')) {
