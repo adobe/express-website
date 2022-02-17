8b8cbb5c7ab525ae5e267ca79c906c8feea23d2b
dyland <dylandepass@gmail.com>
2022-02-17 13:55:53 -0500

    fix: use $ to be inline with codebase

--- express/blocks/submit-email/submit-email.js
+++ express/blocks/submit-email/submit-email.js
@@ -10,56 +10,55 @@
  * governing permissions and limitations under the License.
  */
 
-function decorateSubmitEmailBlock(block) {
-  const container = document.querySelector('.submit-email-container');
+function decorateSubmitEmailBlock($block) {
+  const $container = document.querySelector('.submit-email-container');
 
-  const icon = container.querySelector('.icon-creative-cloud-express, .icon-cc-express-logo');
+  const $icon = $container.querySelector('.icon-creative-cloud-express, .icon-cc-express-logo');
 
   // Remove submit-email block wrapping div
-  const blockParentDiv = block.parentElement;
-  container.insertBefore(block, container.firstChild);
-  container.removeChild(blockParentDiv);
-  icon.setAttribute('src', '/express/icons/creative-cloud-express.svg');
+  const $blockParentDiv = $block.parentElement;
+  $container.insertBefore($block, $container.firstChild);
+  $container.removeChild($blockParentDiv);
+  $icon.setAttribute('src', '/express/icons/creative-cloud-express.svg');
 
-  const heading = container.querySelector('h1');
+  const $heading = $container.querySelector('h1');
 
-  const form = document.createElement('form');
-  const formHeading = document.createElement('h2');
-  formHeading.textContent = 'Subscribe Now';
-  formHeading.classList.add('form-heading');
+  const $form = document.createElement('form');
+  const $formHeading = document.createElement('h2');
+  $formHeading.textContent = 'Subscribe Now';
+  $formHeading.classList.add('form-heading');
 
-  const emailInput = document.createElement('input');
-  emailInput.classList.add('email-input');
-  emailInput.setAttribute('type', 'email');
-  emailInput.setAttribute('placeholder', 'Email');
+  const $emailInput = document.createElement('input');
+  $emailInput.classList.add('email-input');
+  $emailInput.setAttribute('type', 'email');
+  $emailInput.setAttribute('placeholder', 'Email');
 
-  const submitButton = document.createElement('a');
-  submitButton.textContent = 'Submit';
-  submitButton.setAttribute('href', '');
-  submitButton.classList.add('button');
-  submitButton.classList.add('accent');
-  submitButton.addEventListener('click', (e) => {
+  const $submitButton = document.createElement('a');
+  $submitButton.textContent = 'Submit';
+  $submitButton.setAttribute('href', '');
+  $submitButton.classList.add('button');
+  $submitButton.classList.add('accent');
+  $submitButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const email = emailInput.value;
+    const email = $emailInput.value;
     if (email) {
       // TODO: Send email to server
-      formHeading.textContent = 'Thanks for signing up!';
-      formHeading.classList.add('success');
+      $formHeading.textContent = 'Thanks for signing up!';
+      $formHeading.classList.add('success');
     }
   });
 
-  const formBlock = document.createElement('div');
-  formBlock.classList.add('form-block');
-  formBlock.appendChild(emailInput);
-  formBlock.appendChild(submitButton);
-  form.appendChild(formHeading);
-  form.appendChild(formBlock);
+  const $formBlock = document.createElement('div');
+  $formBlock.classList.add('form-block');
+  $formBlock.appendChild($emailInput);
+  $formBlock.appendChild($submitButton);
+  $form.appendChild($formHeading);
+  $form.appendChild($formBlock);
 
-  heading.after(form);
+  $heading.after($form);
 
   // Change p to spans
-  const paragraphs = block.querySelectorAll('p');
-  for (const p of paragraphs) {
+  for (const p of $block.querySelectorAll('p')) {
     const span = document.createElement('span');
     span.innerHTML = p.innerHTML;
     p.parentNode.replaceChild(span, p);
