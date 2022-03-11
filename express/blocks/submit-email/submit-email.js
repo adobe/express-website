4ea36c8c6723c250b4def68a273bd6adaa989551
dyland <dylandepass@gmail.com>
2022-03-10 21:28:57 -0500

    fix: remove stress test logic

--- express/blocks/submit-email/submit-email.js
+++ express/blocks/submit-email/submit-email.js
@@ -10,10 +10,6 @@
  * governing permissions and limitations under the License.
  */
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default function decorate($block) {
   const $container = document.querySelector('.submit-email-container');
 
@@ -45,34 +41,14 @@ export default function decorate($block) {
   $submitButton.setAttribute('href', '');
   $submitButton.classList.add('button');
   $submitButton.classList.add('accent');
-  $submitButton.addEventListener('click', async (e) => {
+  $submitButton.addEventListener('click', (e) => {
     e.preventDefault();
+    const email = $emailInput.value;
 
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
+    if (email && $form.checkValidity()) {
+      const headers = new Headers();
+      headers.append('Content-Type', 'application/json');
 
-    // Start time of test
-    const testStart = new Date().getTime();
-
-    // Start time of current chunk
-    let chunkStart = new Date().getTime();
-
-    // Total sent
-    let total = 0;
-
-    // Index in current chunk
-    let index = 0;
-
-    const failed = [];
-
-    const params = new URL(window.location).searchParams;
-    const TOTAL_TEST_MESSAGES = Math.min(params.get('total'), 100000);
-    const CHUNK_SIZE = Math.min(params.get('chunkSize'), 2000);
-    const key = params.get('key') || 'test';
-    const SLEEP = params.get('sleep') || 30;
-
-    for (let i = 0; i < TOTAL_TEST_MESSAGES; i += 1) {
-      const email = `adobe-test-${total}-${key}@adobetest.com`;
       const body = {
         sname: 'adbemeta',
         email,
@@ -86,55 +62,19 @@ export default function decorate($block) {
         body: JSON.stringify(body),
       };
 
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        await fetch('https://www.adobe.com/api2/subscribe_v1', requestOptions);
-        console.log(`Sent ${index} email to: ${email}`);
-        $formHeading.textContent = 'Thanks for signing up!';
-        $formHeading.classList.add('success');
-        $emailInput.classList.remove('error');
-        $emailInput.value = '';
-      } catch (error) {
-        console.log('Error: ', error.message);
-        $formHeading.textContent = `An error occurred during subscription: ${error.message}`;
-        failed.push(email);
-      }
-
-      total += 1;
-      index += 1;
-
-      // Calculate duration of current chunk
-      const now = new Date().getTime();
-      const duration = now - chunkStart;
-      console.log(`Total Attempted ${total} - Chunk Duration ${duration}ms`);
-
-      // Slight delay
-      // eslint-disable-next-line no-await-in-loop
-      await sleep(SLEEP);
-
-      // If the duration of this chunk is greater than 1 minute or
-      // we have already sent 2000 messages then start a new chunk
-      if (duration > 60000 || index === CHUNK_SIZE) {
-        index = 0;
-        console.log('Hit limit.. pausing');
-
-        // if we finished before the minute mark then wait until the minute mark
-        // eslint-disable-next-line no-await-in-loop
-        await sleep(60000 - duration);
-        chunkStart = new Date().getTime();
-      }
-    }
-
-    // Test complete
-    const now = new Date().getTime();
-    const duration = now - testStart;
-    console.log(`Sent: ${total}, Total Duration: ${duration}ms`);
-
-    if (failed.length > 0) {
-      console.log(`Failed to send (${failed.length}):`);
-      failed.forEach((email) => {
-        console.log(`${email}`);
-      });
+      fetch('https://www.adobe.com/api2/subscribe_v1', requestOptions)
+        .then(() => {
+          $formHeading.textContent = 'Thanks for signing up!';
+          $formHeading.classList.add('success');
+          $emailInput.classList.remove('error');
+          $emailInput.value = '';
+        })
+        .catch(() => {
+          $formHeading.textContent = 'An error occurred during subscription';
+        });
+    } else {
+      $emailInput.classList.add('error');
+      $form.reportValidity();
     }
   });
 
