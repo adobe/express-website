50976856f90f245c0627da7dd7d0960ed237302f
Mason Zhang <12515123+MasonPinZ@users.noreply.github.com>
2022-03-15 17:56:29 +0900

    add some comments

--- express/scripts/bw2.js
+++ express/scripts/bw2.js
@@ -9,6 +9,10 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+/*
+ * Japanese balanced word wrap logic
+ */
+
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 
