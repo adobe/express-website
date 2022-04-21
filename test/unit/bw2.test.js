faad38c3626c1b7ea932702ccb56f22208000f1c
Mason Zhang <12515123+MasonPinZ@users.noreply.github.com>
2022-04-21 15:36:28 +0900

    add unit tests

---
+++ test/unit/bw2.test.js
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* global expect */
+/* eslint-env mocha */
+
+import {
+  createTag,
+} from '../../express/scripts/scripts.js';
+import BalancedWordWrapper from '../../express/scripts/bw2.js';
+
+describe('Japanese balanced word wrap', () => {
+  describe('BalancedWordWrapper', () => {
+    const validate = (target = document.body, expects = []) => {
+      const children = target.childNodes;
+      expect(children.length).to.equal(expects.length, 'number of child nodes mismatch');
+      for (const [i, c] of children.entries()) {
+        const e = expects[i];
+        if (typeof e === 'string') {
+          expect(c.nodeType).to.equal(Node.TEXT_NODE);
+          expect(c.textContent).to.equal(e);
+        } else {
+          expect(c.nodeType).to.equal(Node.ELEMENT_NODE);
+          expect(c.nodeName.toLowerCase()).to.equal(e.nodeName.toLowerCase());
+          if (e.classList) {
+            expect(Array.from(c.classList)).to.have.members(e.classList.map((v) => `jpn-balanced-wbr-l${v}`));
+          }
+        }
+      }
+    };
+
+    it('should insert proper class names to WBR tags', () => {
+      let bw2 = new BalancedWordWrapper();
+      const t1 = createTag('h1');
+      t1.innerHTML = 'aaa<wbr>bbb';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa',
+        { nodeName: 'wbr', classList: [1, 2, 3, 4, 5] },
+        'bbb',
+      ]);
+      bw2 = new BalancedWordWrapper(2);
+      t1.innerHTML = 'aaa<wbr>bbb<wbr>ccc';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa',
+        { nodeName: 'wbr', classList: [1, 2] },
+        'bbb',
+        { nodeName: 'wbr', classList: [2] },
+        'ccc',
+      ]);
+      t1.innerHTML = 'aaa<wbr>bbbb<wbr>ccc';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa',
+        { nodeName: 'wbr', classList: [1, 2] },
+        'bbbb',
+        { nodeName: 'wbr', classList: [2] },
+        'ccc',
+      ]);
+      t1.innerHTML = 'aa<wbr>bbbb<wbr>ccc';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aa',
+        { nodeName: 'wbr', classList: [2] },
+        'bbbb',
+        { nodeName: 'wbr', classList: [1, 2] },
+        'ccc',
+      ]);
+      t1.innerHTML = 'aaa<wbr>bbb<wbr>ccc<wbr>ddd<wbr>eee<wbr>fff';
+      bw2 = new BalancedWordWrapper();
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa',
+        { nodeName: 'wbr', classList: [3, 4, 5] },
+        'bbb',
+        { nodeName: 'wbr', classList: [2, 5] },
+        'ccc',
+        { nodeName: 'wbr', classList: [1, 3, 4, 5] },
+        'ddd',
+        { nodeName: 'wbr', classList: [2, 4, 5] },
+        'eee',
+        { nodeName: 'wbr', classList: [3, 4, 5] },
+        'fff',
+      ]);
+    });
+
+    it('should replace alternative separators with WBR tags', () => {
+      const bw2 = new BalancedWordWrapper();
+      const t1 = createTag('h1');
+      t1.innerHTML = 'aaa\uff3fbbb';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa', { nodeName: 'wbr' }, 'bbb',
+      ]);
+      t1.innerHTML = 'aaa\uff3f\uff3fbbb';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa', { nodeName: 'wbr' }, 'bbb',
+      ]);
+      t1.innerHTML = 'aaa\uff3f<wbr>bbb';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa', { nodeName: 'wbr' }, 'bbb',
+      ]);
+      t1.innerHTML = 'aaa\uff3fbbb<wbr>ccc';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa', { nodeName: 'wbr' }, 'bbb', { nodeName: 'wbr' }, 'ccc',
+      ]);
+      t1.innerHTML = 'aaa\uff3fbbb\uff3f<wbr>ccc';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa', { nodeName: 'wbr' }, 'bbb', { nodeName: 'wbr' }, 'ccc',
+      ]);
+      t1.innerHTML = 'aaa\uff3fbbb\uff3fccc';
+      bw2.applyElement(t1);
+      validate(t1, [
+        'aaa', { nodeName: 'wbr' }, 'bbb', { nodeName: 'wbr' }, 'ccc',
+      ]);
+    });
+  });
+});
