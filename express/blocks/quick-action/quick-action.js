28ff62e75cfd7b03245cc4b5387fcc66dc963963
David Nuescheler <david@nuescheler.ch>
2022-05-12 15:29:02 -0600

    chore: switch back to stage

--- express/blocks/quick-action/quick-action.js
+++ express/blocks/quick-action/quick-action.js
@@ -22,5 +22,5 @@ export default function decorate(block) {
   // eslint-disable-next-line no-underscore-dangle
   window._sparkImsOnReadyCalled = true;
   block.innerHTML = '<div id="quick-task-container"></div>';
-  loadScript('https://express.adobe.com/static/platform-shell/standalone-host-app-e38b7076.js');
+  loadScript('https://express-stage.adobe.com/static/platform-shell/standalone-host-app-1fecd562.js');
 }
