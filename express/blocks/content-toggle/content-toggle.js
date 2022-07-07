d8f1a742498fa097e28da4d44e05bac3cf3a8cdb
William Ste-Marie <williambsm@hotmail.fr>
2022-07-07 17:17:31 -0400

    feat(content-toggle): basic system

---
+++ express/blocks/content-toggle/content-toggle.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import {
+  createTag,
+// eslint-disable-next-line import/no-unresolved
+} from '../../scripts/scripts.js';
+
+export default function decorate($block) {
+  const $sections = document.querySelectorAll('[data-toggle]');
+  const $toggles = $block.querySelectorAll('li');
+
+  if ($toggles) {
+    $toggles.forEach(($toggle, index) => {
+      if (index === 0) {
+        $toggle.classList.add('active');
+      }
+
+      $toggle.addEventListener('click', (e) => {
+        $sections.forEach(($section) => {
+          if ($section.dataset.toggle === $toggle.textContent) {
+            $section.style.display = 'block';
+          } else {
+            $section.style.display = 'none';
+          }
+        });
+      });
+    });
+  }
+
+  if ($sections) {
+    $sections.forEach(($section, index) => {
+      if (index > 0) {
+        $section.style.display = 'none';
+      }
+    });
+  }
+}
