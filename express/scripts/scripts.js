ac2ef52643dbf85e4566aaca17a1753259926476
Solène Duprat <solene.duprat@gmail.com>
2022-07-15 10:17:03 -0400

    feat(banner): banner script from branch.io

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -2060,6 +2060,18 @@ export async function addFreePlanWidget(elem) {
 }
 
 /**
+ * banner script from branch.io
+ */
+async function addBannerToPage() {
+  if (['yes', 'true'].includes(getMetadata('show-banner').toLowerCase())) {
+    // eslint-disable-next-line
+    (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking".split(" "), 0);
+    // eslint-disable-next-line
+    branch.init('key_live_ikqc03rxCzFIX8gq8i2nBddbquai90pm');
+  }
+}
+
+/**
  * loads everything that doesn't need to be delayed.
  */
 async function loadLazy() {
@@ -2073,6 +2085,7 @@ async function loadLazy() {
   scrollToHash();
   resolveFragments();
   addPromotion();
+  addBannerToPage();
   removeMetadata();
   addFavIcon('/express/icons/cc-express.svg');
   if (!window.hlx.lighthouse) loadMartech();
