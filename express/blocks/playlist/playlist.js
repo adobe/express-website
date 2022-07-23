8540636dc4cf70b1b2f3c97e0f1ab9224842745a
Qiyun Dai <pianoquin@gmail.com>
2022-07-22 20:35:59 -0500

    feat(playlist): sessions object building done. starting on rendering.

--- express/blocks/playlist/playlist.js
+++ express/blocks/playlist/playlist.js
@@ -16,43 +16,80 @@ import { createTag } from '../../scripts/scripts.js';
 function loadVideo(videoArr) {
   // In this function, replace the current video with a new one or load the first video.
   // eslint-disable-next-line no-console
-  console.log(videoArr);
+  console.log('Video array:', videoArr);
 }
 
 function loadSection(sectionObj) {
   // In this function, replace the current section with a new one.
   // eslint-disable-next-line no-console
-  console.log(sectionObj);
+  console.log('sectionObj: ', sectionObj);
 
   loadVideo();
 }
 
 export default function decorate($block) {
-  const videos = [
-    {
-      title: 'Session 1',
-      subtitle: 'Promote your dogs',
-      videos: [],
-    },
-  ];
+  const sessions = [];
+  let sessionIndex = -1;
 
   Array.from($block.children).forEach(($row) => {
+    const $sessionThumbnail = $row.querySelector('picture');
+    const $sessionTitle = $row.querySelector('h3');
+    const $sessionDescription = $row.querySelector('h2');
+    const $videosThumbnail = $row.querySelector('picture');
+    const $videos = $row.querySelectorAll('a');
+    const $nodes = $row.querySelectorAll('div>div');
+    const $videoDuration = $nodes[$nodes.length - 1].textContent;
     // If the row is a session title, then create a new array of videos.
+
     // If the row is a video, then add the video to the current session array.
 
-    // eslint-disable-next-line no-console
-    console.log($row);
-    // eslint-disable-next-line no-console
-    console.log(videos);
+    if ($sessionTitle) {
+      console.log($row);
+      sessionIndex += 1;
+
+      sessions.push({
+        title: $sessionTitle.textContent,
+        description: $sessionDescription.textContent,
+        thumbnail: $sessionThumbnail,
+        videos: [],
+      });
+    }
+
+    if ($videosThumbnail) {
+      sessions[sessionIndex].videos.push({
+        title: $videos[0].textContent,
+        files: Array.from($videos, (a) => a.href),
+        thumbnail: $videosThumbnail,
+        duration: $videoDuration,
+      });
+    }
   });
+
+  console.log(sessions);
 
   $block.innerHTML = '';
 
   // Rebuild the whole block properly.
-  const $playlistContainer = createTag('div', { class: 'playlist-container' });
-  $playlistContainer.textContent = 'Hello world';
-  $block.append($playlistContainer);
+  const $thumbnailsContainer = createTag('div', { class: 'thumbnails-container' });
+  $block.append($thumbnailsContainer);
+
+  for (let i = 0; i < sessions.length; i += 1) {
+    const $sessionBlock = createTag('div', { class: 'session-block' });
+    $thumbnailsContainer.append($sessionBlock);
+
+    const $sessionThumbnail = sessions[i].thumbnail;
+    const $sessionTitle = createTag('h5', { class: 'session-title' });
+    $sessionTitle.textContent = sessions[i].title;
+    const $sessionDescription = createTag('h4', { class: 'session-description' });
+    $sessionDescription.textContent = sessions[i].description;
+    $sessionBlock.append($sessionThumbnail);
+    $sessionBlock.append($sessionTitle);
+    $sessionBlock.append($sessionDescription);
+  }
+
+  const $videoPlayer = createTag('div', { class: 'video-player' });
+  $block.append($videoPlayer);
 
   // Load the latest section and video.
-  loadSection(videos[videos.length - 1]);
+  loadSection(sessions[sessions.length - 1]);
 }
