e953f7e3e717966edc9306f50439232a360564c5
William Ste-Marie <williambsm@hotmail.fr>
2022-08-30 11:21:59 -0400

    feat(templates-api): top templates

--- express/scripts/templates.js
+++ express/scripts/templates.js
@@ -19,10 +19,29 @@ async function fetchPageContent(path) {
   return window.templates.data.find((p) => p.path === path && p.live === 'Y');
 }
 
+function updateLinkList(container, template, list) {
+  const templatePages = window.templates.data ?? [];
+  container.innerHTML = '';
+
+  if (list && templatePages) {
+    list.forEach((d) => {
+      const templatePageData = templatePages.find((p) => p.name === d && p.live === 'Y');
+
+      if (templatePageData) {
+        const clone = template.cloneNode(true);
+        clone.innerHTML = clone.innerHTML.replace('/express/templates/default', templatePageData.path);
+        clone.innerHTML = clone.innerHTML.replace('Default', templatePageData.name);
+        container.append(clone);
+      }
+    });
+  }
+}
+
 function updateBlocks(data) {
   const heroAnimation = document.querySelector('.hero-animation--wide-');
   const linkList = document.querySelector('.link-list--fullwidth-');
   const templateList = document.querySelector('.template-list--fullwidth--apipowered-');
+  const seoNav = document.querySelector('.seo-nav');
 
   if (heroAnimation) {
     if (data.title) {
@@ -30,7 +49,7 @@ function updateBlocks(data) {
     }
 
     if (data.text) {
-      heroAnimation.innerHTML = heroAnimation.innerHTML.replace('Default template description', data.text);
+      heroAnimation.innerHTML = heroAnimation.innerHTML.replace('Default template text', data.text);
     }
   }
 
@@ -39,20 +58,7 @@ function updateBlocks(data) {
     const linkListTemplate = linkList.querySelector('p').cloneNode(true);
     const linkListData = data.related.split(', ');
 
-    linkListContainer.innerHTML = '';
-
-    if (linkListData) {
-      linkListData.forEach((d) => {
-        const templatePageData = window.templates.data.find((p) => p.path.includes(d) && p.live === 'Y');
-
-        if (templatePageData) {
-          const linkListClone = linkListTemplate.cloneNode(true);
-          linkListClone.innerHTML = linkListClone.innerHTML.replace('/express/templates/default', templatePageData.path);
-          linkListClone.innerHTML = linkListClone.innerHTML.replace('Default', templatePageData.name);
-          linkListContainer.append(linkListClone);
-        }
-      });
-    }
+    updateLinkList(linkListContainer, linkListTemplate, linkListData);
   }
 
   if (templateList) {
@@ -64,6 +70,24 @@ function updateBlocks(data) {
 
     if (data.premium) {
       templateList.innerHTML = templateList.innerHTML.replace('default-premium', data.premium);
+    }
+  }
+
+  if (seoNav) {
+    if (window.templates.data && data.topTemplates) {
+      const topTemplatesContainer = seoNav.querySelector('p').parentElement;
+      const topTemplatesTemplate = seoNav.querySelector('p').cloneNode(true);
+      const topTemplatesData = data.topTemplates.split(', ');
+
+      updateLinkList(topTemplatesContainer, topTemplatesTemplate, topTemplatesData);
+    }
+
+    if (data.topTemplatesTitle) {
+      seoNav.innerHTML = seoNav.innerHTML.replace('Default top templates title', data.topTemplatesTitle);
+    }
+
+    if (data.topTemplatesText) {
+      seoNav.innerHTML = seoNav.innerHTML.replace('Default top templates text', data.topTemplatesText);
     }
   }
 }
