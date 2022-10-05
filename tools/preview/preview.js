5589c1cfe4626edd723995391a43411085c17875
Lars Trieloff <lars@trieloff.net>
2022-10-05 15:50:44 +0200

    feat(experiments): show more data in experiment tool overlay

--- tools/preview/preview.js
+++ tools/preview/preview.js
@@ -33,6 +33,32 @@ function createTesting() {
   return '';
 }
 
+const percentformat = new Intl.NumberFormat('en-US', { style: 'percent', maximumSignificantDigits: 2 });
+const countformat = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 2 });
+const significanceformat = {
+  format: (value) => {
+    if (value < 0.005) {
+      return 'highly significant';
+    } else if (value < 0.05) {
+      return 'significant';
+    } else if (value < 0.1) {
+      return 'marginally significant';
+    }
+    return 'not significant';
+  },
+};
+const bigcountformat = {
+  format: (value) => {
+    if (value > 1000000) {
+      return `${countformat.format(value / 1000000)}M`;
+    }
+    if (value > 1000) {
+      return `${countformat.format(value / 1000)}K`;
+    }
+    return countformat.format(value);
+  },
+};
+
 /**
  * Create Badge if a Page is enlisted in a Helix Experiment
  * @return {Object} returns a badge or empty string
@@ -47,7 +73,7 @@ async function createExperiment() {
       const variant = config.variants[variantName];
       const split = +variant.percentageSplit
         || 1 - config.variantNames.reduce((c, vn) => c + +config.variants[vn].percentageSplit, 0);
-      const percentage = Math.round(split * 10000) / 100;
+      const percentage = percentformat.format(split);
       const div = document.createElement('div');
 
       const experimentURL = new URL(window.location.href);
@@ -56,9 +82,9 @@ async function createExperiment() {
 
       div.className = `hlx-variant${selectedVariant === variantName ? ' hlx-variant-selected' : ' '}`;
       div.innerHTML = `<div>
-      <h5>${variantName}</h5>
+      <h5><code>${variantName}</code></h5>
         <p>${variant.label}</p>
-        <p>${percentage}%</p>
+        <p class="percentage">(${percentage} split)</p>
         <p class="performance"></p>
       </div>
       <div class="hlx-button"><a href="${experimentURL.href}">Simulate</a></div>`;
@@ -75,7 +101,8 @@ async function createExperiment() {
       <div class="hlx-popup-header">
         <div>
           <h4>${config.experimentName}</h4>
-          <div class="hlx-details">${config.status}, ${config.audience}, Blocks: ${config.variants.control.blocks.join(',')}</div>
+          <div class="hlx-details">${config.status}${config.audience ? ', ' : ''}${config.audience}${config.variants.control.blocks.length ? ', Blocks: ' : ''}${config.variants.control.blocks.join(',')}</div>
+          <div class="hlx-info">How is it going?</div>
         </div>
         <div>
         ${manifestButton}
@@ -100,17 +127,97 @@ async function createExperiment() {
       }
       fetch(resultsURL.href).then(async (response) => {
         const { results } = await response.json();
-        results.forEach((result) => {
-          const nf = {
-            format: (num) => `${Math.floor(num)}%`,
-          };
+
+        const numberify = (obj) => Object.entries(obj).reduce((o, [k, v]) => {
+          o[k] = Number.parseFloat(v);
+          o[k] = Number.isNaN(o[k]) ? v : o[k];
+          return o;
+        }, {});
+
+        const variantsAsNums = results.map(numberify);
+        const totals = Object.entries(variantsAsNums.reduce((o, v) => {
+          Object.entries(v).forEach(([k, val]) => {
+            if (typeof val === 'number' && Number.isInteger(val) && k.startsWith('variant_')) {
+              o[k] = (o[k] || 0) + val;
+            } else if (typeof val === 'number' && Number.isInteger(val) && k.startsWith('control_')) {
+              o[k] = val;
+            }
+          });
+          return o;
+        }, {})).reduce((o, [k, v]) => {
+          o[k] = v;
+          const vkey = k.replace(/^(variant|control)_/, 'variant_');
+          const ckey = k.replace(/^(variant|control)_/, 'control_');
+          const tkey = k.replace(/^(variant|control)_/, 'total_');
+          if (o[ckey] && o[vkey]) {
+            o[tkey] = o[ckey] + o[vkey];
+          }
+          return o;
+        }, {});
+        const richVariants = variantsAsNums.map((v) => ({
+          ...v,
+          allocation_rate: v.variant_experimentations / totals.total_experimentations,
+        })).reduce((o, v) => {
+          const variantName = v.variant;
+          o[variantName] = v;
+          return o;
+        }, {
+          control: {
+            variant: 'control',
+            ...Object.entries(variantsAsNums[0]).reduce((k, v) => {
+              const [key, val] = v;
+              if (key.startsWith('control_')) {
+                k[key.replace(/^control_/, 'variant_')] = val;
+              }
+              return k;
+            }, {}),
+          },
+        });
+        const winner = variantsAsNums.reduce((w, v) => {
+          console.log(v);
+          if (v.variant_conversion_rate > w.conversion_rate && v.p_value < 0.05) {
+            w.conversion_rate = v.variant_conversion_rate;
+            w.p_value = v.p_value;
+            w.variant = v.variant;
+          }
+          return w;
+        }, { variant: 'control', p_value: 1, conversion_rate: 0 });
+        console.log('winner', winner);
+
+        // add summary
+        const summary = div.querySelector('.hlx-info');
+        summary.innerHTML = `Showing results for ${bigcountformat.format(totals.total_experimentations)} visits and ${bigcountformat.format(totals.total_conversions)} conversions: `;
+        if (totals.total_conversion_events < 500 && winner.p_value > 0.05) {
+          console.log('totals', totals);
+          summary.innerHTML += ` not yet enough data to determine a winner. Keep going until you get ${bigcountformat.format((500 * totals.total_experimentations) / totals.total_conversion_events)} visits.`;
+        } else if (winner.p_value > 0.05) {
+          summary.innerHTML += ' no significant difference between variants. In doubt, stick with <code>control</code>.';
+        } else if (winner.variant === 'control') {
+          summary.innerHTML += ' Stick with <code>control</code>. No variant is better than the control.';
+        } else {
+          summary.innerHTML += ` <code>${winner.variant}</code> is the winner.`;
+        }
+
+        // add traffic allocation to control and each variant
+        Object.keys(variantMap).forEach((variantName) => {
+          const variantDiv = variantMap[variantName];
+          const percentage = variantDiv.querySelector('.percentage');
+          percentage.innerHTML = `
+            <span title="${countformat.format(richVariants[variantName].variant_conversion_events)} real events">${bigcountformat.format(richVariants[variantName].variant_conversions)} clicks</span> /
+            <span title="${countformat.format(richVariants[variantName].variant_experimentation_events)} real events">${bigcountformat.format(richVariants[variantName].variant_experimentations)} visits</span>
+            <span>(${percentformat.format(richVariants[variantName].variant_experimentations / totals.total_experimentations)} split)</span>
+          `;
+        });
+
+        // add click rate and significance to each variant
+        variantsAsNums.forEach((result) => {
           const variant = variantMap[result.variant];
           if (variant) {
             const performance = variant.querySelector('.performance');
             performance.innerHTML = `
-              <span>click rate: ${nf.format(100 * Number.parseFloat(result.variant_conversion_rate))}</span>
-              <span>vs. ${nf.format(100 * Number.parseFloat(result.control_conversion_rate))}</span>
-              <span>significance: ${nf.format(100 * Number.parseFloat(result.p_value))}</span> <!-- everything below 95% is not good enough for the social sciences to be considered significant --->
+              <span>click rate: ${percentformat.format(result.variant_conversion_rate)}</span>
+              <span>vs. ${percentformat.format(result.control_conversion_rate)}</span>
+              <span title="p value: ${result.p_value}" class="significance ${significanceformat.format(result.p_value).replace(/ /, '-')}">${significanceformat.format(result.p_value)}</span>
             `;
           }
         });
