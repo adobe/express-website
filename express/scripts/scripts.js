4e2b9c616fd917222d2e6ce9df00f569a7cb7142
Lars Trieloff <lars@trieloff.net>
2022-10-06 15:20:48 +0200

    feat(rum): increase RUM sampling rate for pages with active experiments

--- express/scripts/scripts.js
+++ express/scripts/scripts.js
@@ -11,7 +11,9 @@
  */
 /* eslint-disable no-console */
 
-window.RUM_GENERATION = 'ccx-gen-4-boilerplate';
+window.RUM_GENERATION = 'ccx-gen-4-experiment-high-sample-rate';
+window.RUM_LOW_SAMPLE_RATE = 100;
+window.RUM_HIGH_SAMPLE_RATE = 10;
 
 /**
  * log RUM if part of the sample.
@@ -37,11 +39,12 @@ export function sampleRUM(checkpoint, data = {}) {
   };
   defer('observe');
   defer('cwv');
+  defer('stash');
   try {
     window.hlx = window.hlx || {};
     if (!window.hlx.rum) {
       const usp = new URLSearchParams(window.location.search);
-      const weight = (usp.get('rum') === 'on') ? 1 : 100; // with parameter, weight is 1. Defaults to 100.
+      const weight = (usp.get('rum') === 'on') ? 1 : window.RUM_LOW_SAMPLE_RATE;
       // eslint-disable-next-line no-bitwise
       const hashCode = (s) => s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0);
       const id = `${hashCode(window.location.href)}-${new Date().getTime()}-${Math.random().toString(16).substr(2, 14)}`;
@@ -50,16 +53,19 @@ export function sampleRUM(checkpoint, data = {}) {
       // eslint-disable-next-line object-curly-newline
       window.hlx.rum = { weight, id, random, isSelected, sampleRUM };
     }
-    const { weight, id } = window.hlx.rum;
-    if (window.hlx && window.hlx.rum && window.hlx.rum.isSelected) {
+    const { id } = window.hlx.rum;
+    if ((window.hlx && window.hlx.rum && window.hlx.rum.isSelected) || checkpoint === 'experiment') {
       const sendPing = (pdata = data) => {
+        if (!window.hlx.rum.isSelected) {
+          return;
+        }
         // eslint-disable-next-line object-curly-newline, max-len, no-use-before-define
-        const body = JSON.stringify({ weight, id, referer: window.location.href, generation: window.RUM_GENERATION, checkpoint, ...data });
-        const url = `https://rum.hlx.page/.rum/${weight}`;
+        const body = JSON.stringify({ weight: window.hlx.rum.weight, id, referer: window.location.href, generation: window.RUM_GENERATION, checkpoint, ...data });
+        const url = `https://rum.hlx.page/.rum/${window.hlx.rum.weight}`;
         // eslint-disable-next-line no-unused-expressions
         navigator.sendBeacon(url, body);
         // eslint-disable-next-line no-console
-        console.debug(`ping:${checkpoint}`, pdata);
+        console.debug(`ping:${checkpoint}:${window.hlx.rum.weight}`, pdata);
       };
       sampleRUM.cases = sampleRUM.cases || {
         cwv: () => sampleRUM.cwv(data) || true,
@@ -71,11 +77,22 @@ export function sampleRUM(checkpoint, data = {}) {
           sendPing(data);
           return true;
         },
+        experiment: () => {
+          // track experiments with higher sampling rate
+          window.hlx.rum.weight = Math.min(window.hlx.rum.weight, window.RUM_HIGH_SAMPLE_RATE);
+          window.hlx.rum.isSelected = (window.hlx.rum.random * window.hlx.rum.weight < 1);
+
+          sampleRUM.drain('stash', sampleRUM);
+          sendPing(data);
+          return true;
+        },
       };
       sendPing(data);
       if (sampleRUM.cases[checkpoint]) {
         sampleRUM.cases[checkpoint]();
       }
+    } else {
+      sampleRUM.stash(checkpoint, data); // save the event for later
     }
   } catch (error) {
     // something went wrong
