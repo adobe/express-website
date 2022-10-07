a2b733b505348c3a207f31a15b871493070148e6
Julien Ramboz <ramboz@adobe.com>
2022-10-07 16:37:23 +0200

    feat: table of contents for mobile web (CCX0074)

--- express/experiments/ccx0074/blocks/toc1/toc.js
+++ express/experiments/ccx0074/blocks/toc1/toc.js
@@ -1,14 +1,31 @@
+
 export default function decorate($block) {
   $block.parentElement.addEventListener('click', (ev) => {
     if (ev.target === $block.parentElement) {
-      $block.parentElement.classList.toggle('open', false);
+      $block.parentElement.parentElement.classList.toggle('open', false);
     }
   });
 
   const toggle = document.querySelector('.default-content-wrapper .button.accent');
+  toggle.classList.remove('accent');
   toggle.href = '#toc';
   toggle.target = '';
   toggle.addEventListener('click', (ev) => {
-    $block.parentElement.classList.toggle('open');
+    $block.parentElement.parentElement.classList.toggle('open');
   });
+
+  const heading = document.createElement('h2');
+  heading.classList.add('toc-heading');
+  heading.innerText = toggle.innerText;
+  $block.insertBefore(heading, $block.firstChild);
+
+  const toggle2 = document.createElement('a');
+  toggle2.classList.add('button');
+  toggle2.classList.add('toc-close');
+  toggle2.href = '#toc';
+  toggle2.innerText = 'Close';
+  toggle2.addEventListener('click', (ev) => {
+    $block.parentElement.parentElement.classList.toggle('open', false);
+  });
+  $block.append(toggle2);
 }
