08dcdedf5e5410cd4fad794375e2a76d0d9e2e11
Julien Ramboz <ramboz@adobe.com>
2022-10-07 15:49:54 +0200

    feat: table of contents for mobile web (CCX0074)

--- express/experiments/ccx0074/blocks/toc1/toc.js
+++ express/experiments/ccx0074/blocks/toc1/toc.js
@@ -1,3 +1,14 @@
 export default function decorate($block) {
+  $block.parentElement.addEventListener('click', (ev) => {
+    if (ev.target === $block.parentElement) {
+      $block.parentElement.classList.toggle('open', false);
+    }
+  });
 
+  const toggle = document.querySelector('.default-content-wrapper .button.accent');
+  toggle.href = '#toc';
+  toggle.target = '';
+  toggle.addEventListener('click', (ev) => {
+    $block.parentElement.classList.toggle('open');
+  });
 }
