12d08c57af1db0d1317402d1ab5d2754f8f4216b
Qiyun Dai <pianoquin@gmail.com>
2023-01-09 21:41:02 -0600

    feat(floating-panel): interaction done

--- express/blocks/floating-panel/floating-panel.js
+++ express/blocks/floating-panel/floating-panel.js
@@ -11,24 +11,24 @@
  */
 
 import {
-  createTag, fetchPlaceholders, getLottie,
+  createTag,
+  fetchPlaceholders,
+  getLottie,
   lazyLoadLottiePlayer,
 } from '../../scripts/scripts.js';
 
-let scrollState = 'withLottie';
-
 const hideScrollArrow = ($floatButtonWrapper, $lottieScrollButton) => {
-  $floatButtonWrapper.classList.add('floating-button--scrolled');
+  $floatButtonWrapper.classList.add('scrolled');
   if (document.activeElement === $lottieScrollButton) $lottieScrollButton.blur();
   $lottieScrollButton.tabIndex = -1;
 };
 
 const showScrollArrow = ($floatButtonWrapper, $lottieScrollButton) => {
-  $floatButtonWrapper.classList.remove('floating-button--scrolled');
+  $floatButtonWrapper.classList.remove('scrolled');
   $lottieScrollButton.removeAttribute('tabIndex');
 };
 
-function initLottieArrow($lottieScrollButton, buttonWrapper, $scrollAnchor) {
+function initLottieArrow($lottieScrollButton, buttonWrapper, $scrollAnchor, section) {
   let clicked = false;
   $lottieScrollButton.addEventListener('click', () => {
     clicked = true;
@@ -44,13 +44,18 @@ function initLottieArrow($lottieScrollButton, buttonWrapper, $scrollAnchor) {
     }, 200);
     hideScrollArrow(buttonWrapper, $lottieScrollButton);
   });
+
   window.addEventListener('scroll', () => {
-    scrollState = buttonWrapper.classList.contains('floating-button--scrolled') ? 'withoutLottie' : 'withLottie';
-    const multiFunctionButtonOpened = buttonWrapper.classList.contains('toolbox-opened');
+    if (window.scrollY > 10) {
+      section.classList.add('appear');
+    } else {
+      section.classList.remove('appear');
+    }
+
     if (clicked) return;
     if ($scrollAnchor.getBoundingClientRect().top < 100) {
       hideScrollArrow(buttonWrapper, $lottieScrollButton);
-    } else if (!multiFunctionButtonOpened) {
+    } else {
       showScrollArrow(buttonWrapper, $lottieScrollButton);
     }
   }, { passive: true });
@@ -61,10 +66,9 @@ async function decorateLottieButton(block) {
   const $lottieScrollButton = createTag('button', { class: 'floating-panel-lottie' });
 
   $lottieScrollButton.innerHTML = getLottie('purple-arrows', '/express/blocks/floating-panel/purple-arrows.json');
-  fetchPlaceholders()
-    .then((placeholders) => {
-      $lottieScrollButton.setAttribute('aria-label', placeholders['see-more']);
-    });
+  fetchPlaceholders().then((placeholders) => {
+    $lottieScrollButton.setAttribute('aria-label', placeholders['see-more']);
+  });
 
   if (buttonContainers.length > 0) {
     buttonContainers[buttonContainers.length - 1].append($lottieScrollButton);
@@ -76,9 +80,51 @@ async function decorateLottieButton(block) {
   };
 }
 
+function isInViewport(rect) {
+  return (
+    rect.top >= 0
+    && rect.left >= 0
+    && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function togglePanel(section, otherCTAs) {
+  let CTAsOffBounds = 0;
+
+  for (let i = 0; i < otherCTAs.length; i += 1) {
+    const rect = otherCTAs[i].getBoundingClientRect();
+    if (!isInViewport(rect)) {
+      CTAsOffBounds += 1;
+    }
+  }
+
+  if (CTAsOffBounds === otherCTAs.length) {
+    section.classList.add('appear');
+  } else {
+    section.classList.remove('appear');
+  }
+}
+function initCTAWatcher(section) {
+  const buttons = section.querySelectorAll('a.button');
+
+  if (buttons.length > 0) {
+    const ctaInBlock = buttons[buttons.length - 1];
+    ctaInBlock.classList.add('floating-panel-cta');
+    const otherCTAs = document.querySelectorAll(`a.button[href='${ctaInBlock.href}']:not(.floating-panel-cta)`);
+
+    togglePanel(section, otherCTAs);
+
+    window.addEventListener('resize', () => {
+      togglePanel(section, otherCTAs);
+    }, { passive: true });
+  }
+}
+
 export default async function decorateBlock(block) {
   const button = block.querySelector('p.button-container');
   const heading = block.querySelector('h1, h2, h3, h4, h5, h6');
+  const section = block.closest('.section.floating-panel-container');
 
   if (button) {
     button.parentElement.classList.add('buttons-container');
@@ -87,13 +133,16 @@ export default async function decorateBlock(block) {
   if (heading) {
     heading.parentElement.classList.add('content-container');
   }
+
   lazyLoadLottiePlayer();
-  const ctaElements = decorateLottieButton(block);
+  const ctaElements = await decorateLottieButton(block);
 
   const $scrollAnchor = document.querySelector('.section:not(:nth-child(1)):not(:nth-child(2)) .template-list, .section:not(:nth-child(1)):not(:nth-child(2)) .layouts, .section:not(:nth-child(1)):not(:nth-child(2)) .steps-highlight-container') ?? document.querySelector('.section:nth-child(3)');
   if (!$scrollAnchor) {
     hideScrollArrow(ctaElements.cta, ctaElements.lottie);
   } else {
-    initLottieArrow(ctaElements.lottie, ctaElements.cta, $scrollAnchor);
+    initLottieArrow(ctaElements.lottie, ctaElements.cta, $scrollAnchor, section);
   }
+
+  initCTAWatcher(section);
 }
