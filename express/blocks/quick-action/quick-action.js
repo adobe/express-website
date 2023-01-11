4bba47515b2a20925b160843ed0527c24a91f585
Julien Ramboz <ramboz@adobe.com>
2023-01-11 10:43:52 +0100

    feat: leverage qt host api

--- express/blocks/quick-action/quick-action.js
+++ express/blocks/quick-action/quick-action.js
@@ -12,17 +12,75 @@
 
 import { loadScript } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
-  window.qtId = 'remove-background';
-  window.isWebViewHost = false;
-  window.qtWebViewScheme = 'false';
-  window.qtColorTheme = '';
-  // eslint-disable-next-line no-underscore-dangle
-  window._prjtmrvlsetup = { quickActionRegistryUrl: 'https://express-stage.adobe.com/express-apps/quick-actions-api' };
-  // eslint-disable-next-line no-underscore-dangle
-  window._sparkImsOnReadyCalled = true;
-  block.innerHTML = `<div id="quick-task-container"></div>
-  <cclqt-sign-up-modal id="qt-sign-up-modal"></cclqt-sign-up-modal>
-  <cclqt-done-modal id="qt-done-modal"></cclqt-done-modal>`;
-  loadScript('https://express-stage.adobe.com/static/platform-shell/standalone-host-app-1590e26b.js');
+async function fetchDependency(url, api) {
+  let sharedScriptUrl = usp.get(api) || 'https://custom.adobeprojectm.com/express-apps/ccl-quick-tasks/pr-905/host-shared/entry-f377a22e.js';
+  if (!sharedScriptUrl) {
+    const response = await fetch(url);
+    const json = await response.json();
+    sharedScriptUrl = json.find((api) => api.id === api).entry;
+  }
+}
+
+export default async function decorate(block) {
+  const usp = new URLSearchParams(window.location.search);
+  
+  // FIXME: remove hardcoded fallback once PR is merged to main
+  const sharedScriptUrl = 'https://custom.adobeprojectm.com/express-apps/ccl-quick-tasks/pr-905/host-shared/entry-f377a22e.js'
+    || await fetchDependency('https://express.adobe.com/express-apps/quick-actions-api/host-entries/host-shared', 'host-shared');
+
+  // FIXME: remove hardcoded fallback once PR is merged to main
+  const actionScriptUrl = 'https://custom.adobeprojectm.com/express-apps/ccl-quick-tasks/pr-905/remove-background/entry-00e7f443.js'
+    || await fetchDependency('https://express.adobe.com/express-apps/quick-actions-api', 'host-sharedremove-background');
+
+  window.qtHost = {
+    async qtLoaded(QuickTask) {
+      const task = new QuickTask(document.querySelector('.block.quick-action'), {
+        close() { console.log('[CCLQT CB]', 'close'); },
+        done(options) { console.log('[CCLQT CB]', 'done', options); },
+        navigate(dest, data, file) { console.log('[CCLQT CB]', 'navigate', dest, data, file); },
+        sendEditorStateToHost(state) { console.log('[CCLQT CB]', 'editor-state', state); },
+        sendErrorToHost(err) { console.error('[CCLQT CB]', 'error', err); },
+        navigationData: {
+          config: {
+          'should-use-cloud-storage': true,
+          'preview-only': true
+          }
+        },
+        hostType: 'standalone',
+        browserInfo: { isMobile: false },
+      })
+      window.qtHost.task = task;
+      await task.render();
+      const taskId = window.qtHost.task.qtEle.qtId;
+      task.qtEle.addEventListener(`${taskId}__navigate-to-download`, (ev) => {
+        console.log('[CCLQT EVT]', 'navigate-to-download', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__navigate-to-task`, (ev) => {
+        console.log('[CCLQT EVT]', 'navigate-to-task', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__navigate-to-post-editor`, (ev) => {
+        console.log('[CCLQT EVT]', 'navigate-to-post-editor', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__navigate-to-host`, (ev) => {
+        console.log('[CCLQT EVT]', 'navigate-to-host', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__navigate-to-done-modal`, (ev) => {
+        console.log('[CCLQT EVT]', 'navigate-to-done-modal', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__downloadable-image`, (ev) => {
+        console.log('[CCLQT EVT]', 'downloadable-image', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__close-full-screen-modal`, (ev) => {
+        console.log('[CCLQT EVT]', 'close-full-screen-modal', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__send-error-to-host`, (ev) => {
+        console.log('[CCLQT EVT]', 'send-error-to-host', ev.detail);
+      });
+      task.qtEle.addEventListener(`${taskId}__authenticate-from-host`, (ev) => {
+        console.log('[CCLQT EVT]', 'authenticate-from-host', ev.detail);
+      });
+    }
+  }
+  loadScript(sharedScriptUrl);
+  loadScript(actionScriptUrl);
 }
