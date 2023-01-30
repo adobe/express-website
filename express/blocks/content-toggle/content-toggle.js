9b87af073fa578410caeb1deb30ce1dc051fea9d
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-01-30 17:27:24 -0500

    feat (Content-Toggle Variants): First iteration complete
    Implemented a smaller and dark coloured variant of the content-toggle block

--- express/blocks/content-toggle/content-toggle.js
+++ express/blocks/content-toggle/content-toggle.js
@@ -9,22 +9,39 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import {
-  createTag,
 // eslint-disable-next-line import/no-unresolved
-} from '../../scripts/scripts.js';
+import { createTag } from '../../scripts/scripts.js';
+import { buildCarousel } from '../shared/carousel.js';
 
 export default function decorate($block) {
   const $sections = document.querySelectorAll('[data-toggle]');
   const $toggleContainer = $block.querySelector('ul');
+  let aTag;
 
   $block.innerHTML = '';
 
   Array.from($toggleContainer.children).forEach(($toggle, index) => {
     const $button = createTag('button');
     const section = $toggle.textContent.trim();
+    const tagText = $toggle.textContent.trim().match(/\[(.*?)\]/);
+    const link = $toggle.querySelector('a');
 
     $button.textContent = $toggle.textContent.trim();
+    if ($block.classList.contains('dark') && tagText) {
+      const [fullText, tagTextContent] = tagText;
+      $button.textContent = $toggle.textContent.trim().replace(fullText, '').trim();
+      const $tag = createTag('span', { class: 'tag' });
+      $tag.textContent = tagTextContent;
+      $block.append($tag);
+    }
+
+    if ($block.classList.contains('small') && link) {
+      aTag = createTag('a', { href: link.getAttribute('href') });
+      aTag.append($button);
+      $block.append(aTag);
+    } else {
+      $block.append($button);
+    }
 
     $button.addEventListener('click', () => {
       const $activeButton = $block.querySelector('button.active');
@@ -53,8 +70,6 @@ export default function decorate($block) {
     if (index === 0) {
       $button.classList.add('active');
     }
-
-    $block.append($button);
   });
 
   if ($sections) {
@@ -64,4 +79,15 @@ export default function decorate($block) {
       }
     });
   }
+  buildCarousel('.content-toggle > *', $block);
+
+  const $rightArrow = $block.querySelector('.small .carousel-arrow-right');
+  $rightArrow.addEventListener('click', () => {
+    $block.classList.add('margin-left-zero');
+  });
+
+  const $leftArrow = $block.querySelector('.small .carousel-arrow-left');
+  $leftArrow.addEventListener('click', () => {
+    $block.classList.remove('margin-left-zero');
+  });
 }
