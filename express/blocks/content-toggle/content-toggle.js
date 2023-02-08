0599db226743cddc9e4691a4a7efce4be6905fcc
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-02-08 12:58:12 -0500

    feat (content-toggle): toggle animation implemented

--- express/blocks/content-toggle/content-toggle.js
+++ express/blocks/content-toggle/content-toggle.js
@@ -9,34 +9,52 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import {
-  createTag,
-// eslint-disable-next-line import/no-unresolved
-} from '../../scripts/scripts.js';
+import { createTag } from '../../scripts/scripts.js';
 
 export default function decorate($block) {
   const $sections = document.querySelectorAll('[data-toggle]');
   const $toggleContainer = $block.querySelector('ul');
 
   $block.innerHTML = '';
+  const $toggleBackground = createTag('div', { class: 'toggle-background' });
+  $block.prepend($toggleBackground);
 
   Array.from($toggleContainer.children).forEach(($toggle, index) => {
-    const $button = createTag('button');
-    const section = $toggle.textContent.trim();
-
+    const $button = createTag('button', { class: 'content-toggle-button'});
+    const tagText = $toggle.textContent.trim().match(/\[(.*?)\]/);
     $button.textContent = $toggle.textContent.trim();
+
+    if (tagText) {
+      const [fullText, tagTextContent] = tagText;
+      $button.textContent = $toggle.textContent.trim().replace(fullText, '').trim();
+      const $tag = createTag('span', { class: 'tag' });
+      $tag.textContent = tagTextContent;
+      $button.append($tag);
+      $block.append($button);
+    } else {
+      $block.append($button);
+    }
 
     $button.addEventListener('click', () => {
       const $activeButton = $block.querySelector('button.active');
       const blockPosition = $block.getBoundingClientRect().top;
       const offsetPosition = blockPosition + window.scrollY - 80;
 
+      const activeWidth = $button.offsetWidth + 5;
+      const allButtons = $block.querySelectorAll('.content-toggle-button');
+      let leftOffset = index * 10;
+      for (let i = 0; i < index; i += 1) {
+        leftOffset += allButtons[i].offsetWidth;
+      }
+      $toggleBackground.style.left = `${leftOffset}px`;
+      $toggleBackground.style.width = `${activeWidth}px`;
+
       if ($activeButton !== $toggle) {
         $activeButton.classList.remove('active');
         $button.classList.add('active');
 
         $sections.forEach(($section) => {
-          if (section === $section.dataset.toggle) {
+          if ($section === $section.dataset.toggle) {
             $section.style.display = 'block';
           } else {
             $section.style.display = 'none';
@@ -52,9 +70,14 @@ export default function decorate($block) {
 
     if (index === 0) {
       $button.classList.add('active');
+      const firstButtonWidthGrabbed = setInterval(() => {
+        if ($button.offsetWidth > 0) {
+          $toggleBackground.style.width = `${$button.offsetWidth + 5}px`;
+          $toggleBackground.style.left = 0;
+          clearInterval(firstButtonWidthGrabbed);
+        }
+      }, 200);
     }
-
-    $block.append($button);
   });
 
   if ($sections) {
