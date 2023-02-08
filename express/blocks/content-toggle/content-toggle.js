2c2fb7c5071c2b2d772950ffb8155910036fe813
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-02-08 17:05:54 -0500

    feat (content-toggle): Refactored code & fixed content display

--- express/blocks/content-toggle/content-toggle.js
+++ express/blocks/content-toggle/content-toggle.js
@@ -11,50 +11,50 @@
  */
 import { createTag } from '../../scripts/scripts.js';
 
-export default function decorate($block) {
-  const $sections = document.querySelectorAll('[data-toggle]');
-  const $toggleContainer = $block.querySelector('ul');
+function decorateButton($block, $toggle) {
+  const $button = createTag('button', { class: 'content-toggle-button' });
+  const tagText = $toggle.textContent.trim().match(/\[(.*?)\]/);
 
-  $block.innerHTML = '';
-  const $toggleBackground = createTag('div', { class: 'toggle-background' });
-  $block.prepend($toggleBackground);
-
-  Array.from($toggleContainer.children).forEach(($toggle, index) => {
-    const $button = createTag('button', { class: 'content-toggle-button'});
-    const tagText = $toggle.textContent.trim().match(/\[(.*?)\]/);
+  if (tagText) {
+    const [fullText, tagTextContent] = tagText;
+    const $tag = createTag('span', { class: 'tag' });
+    $button.textContent = $toggle.textContent.trim().replace(fullText, '').trim();
+    $button.dataset.text = $button.textContent.toLowerCase();
+    $tag.textContent = tagTextContent;
+    $button.append($tag);
+  } else {
     $button.textContent = $toggle.textContent.trim();
+    $button.dataset.text = $button.textContent.toLowerCase();
+  }
+  $block.append($button);
+}
 
-    if (tagText) {
-      const [fullText, tagTextContent] = tagText;
-      $button.textContent = $toggle.textContent.trim().replace(fullText, '').trim();
-      const $tag = createTag('span', { class: 'tag' });
-      $tag.textContent = tagTextContent;
-      $button.append($tag);
-      $block.append($button);
-    } else {
-      $block.append($button);
-    }
+function initButton($block, index) {
+  const $enclosingMain = $block.closest('main');
+  if ($enclosingMain) {
+    const $buttons = $block.querySelectorAll('.content-toggle-button');
+    const $sections = $enclosingMain.querySelectorAll('[data-toggle]');
+    const $toggleBackground = $block.querySelector('.toggle-background');
 
-    $button.addEventListener('click', () => {
+    $buttons[index].addEventListener('click', () => {
       const $activeButton = $block.querySelector('button.active');
       const blockPosition = $block.getBoundingClientRect().top;
       const offsetPosition = blockPosition + window.scrollY - 80;
-
-      const activeWidth = $button.offsetWidth + 5;
-      const allButtons = $block.querySelectorAll('.content-toggle-button');
+      const activeButtonWidth = $buttons[index].offsetWidth + 5;
       let leftOffset = index * 10;
+
       for (let i = 0; i < index; i += 1) {
-        leftOffset += allButtons[i].offsetWidth;
+        leftOffset += $buttons[i].offsetWidth;
       }
       $toggleBackground.style.left = `${leftOffset}px`;
-      $toggleBackground.style.width = `${activeWidth}px`;
+      $toggleBackground.style.width = `${activeButtonWidth}px`;
 
-      if ($activeButton !== $toggle) {
+      if ($activeButton !== $buttons[index]) {
         $activeButton.classList.remove('active');
-        $button.classList.add('active');
+        $buttons[index].classList.add('active');
 
         $sections.forEach(($section) => {
-          if ($section === $section.dataset.toggle) {
+          if ($buttons[index].dataset.text === $section.dataset.toggle.toLowerCase()) {
             $section.style.display = 'block';
           } else {
             $section.style.display = 'none';
@@ -67,24 +67,41 @@ export default function decorate($block) {
         });
       }
     });
-
     if (index === 0) {
-      $button.classList.add('active');
+      $buttons[index].classList.add('active');
       const firstButtonWidthGrabbed = setInterval(() => {
-        if ($button.offsetWidth > 0) {
-          $toggleBackground.style.width = `${$button.offsetWidth + 5}px`;
+        if ($buttons[index].offsetWidth > 0) {
+          $toggleBackground.style.width = `${$buttons[index].offsetWidth + 5}px`;
           $toggleBackground.style.left = 0;
           clearInterval(firstButtonWidthGrabbed);
         }
       }, 200);
     }
-  });
+  }
+}
 
-  if ($sections) {
-    $sections.forEach(($section, index) => {
-      if (index > 0) {
-        $section.style.display = 'none';
-      }
+export default function decorate($block) {
+  const $enclosingMain = $block.closest('main');
+  if ($enclosingMain) {
+    const $sections = $enclosingMain.querySelectorAll('[data-toggle]');
+    const $toggleContainer = $block.querySelector('ul');
+    const $toggleBackground = createTag('div', { class: 'toggle-background' });
+
+    $block.innerHTML = '';
+
+    $block.prepend($toggleBackground);
+
+    Array.from($toggleContainer.children).forEach(($toggle, index) => {
+      decorateButton($block, $toggle);
+      initButton($block, index);
     });
+
+    if ($sections) {
+      $sections.forEach(($section, index) => {
+        if (index > 0) {
+          $section.style.display = 'none';
+        }
+      });
+    }
   }
 }
