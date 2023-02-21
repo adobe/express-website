c9a738cc9ec672e595187ada939539f694c8ec48
Qiyun Dai <pianoquin@gmail.com>
2023-02-20 21:34:58 -0600

    feat(paginated-carousel): prototype done

--- express/blocks/paginated-carousel/paginated-carousel.js
+++ express/blocks/paginated-carousel/paginated-carousel.js
@@ -21,23 +21,115 @@ function decorateParagraphs(carouselItem) {
       if (p.querySelectorAll(':scope > picture').length > 0) {
         p.classList.add('image-container');
       }
+
+      if (p.classList.contains('button-container')) {
+        const anchor = p.querySelector('a');
+        if (anchor) {
+          const content = createTag('p', { class: 'content' });
+          content.textContent = anchor.textContent.trim();
+          anchor.innerText = '';
+          carouselItem.append(content);
+        }
+      }
     });
   }
 }
 
-function decoratePagination(block) {
+function decoratePagination(block, payload) {
   const paginationContainer = createTag('div', { class: 'pagination-container' });
   const originalCItems = block.querySelectorAll('.paginated-carousel-item.original');
   if (originalCItems.length > 0) {
-    originalCItems.forEach(())
+    originalCItems.forEach((cItem) => {
+      const dot = createTag('span', { class: 'pagination-dot' });
+      dot.dataset.carouselIndex = cItem.dataset.carouselIndex;
+      paginationContainer.append(dot);
+
+      dot.addEventListener('click', () => {
+        const offset = payload.backwardInfinite ? originalCItems.length * cItem.offsetWidth : 0;
+        block.scrollTo({
+          left: offset + (cItem.offsetWidth * dot.dataset.carouselIndex),
+          behavior: 'smooth',
+        });
+      });
+    });
   }
+
+  block.insertAdjacentElement('afterend', paginationContainer);
 }
 
-function initCarousel(block) {
+function initCarousel(block, payload) {
+  const originalCards = block.querySelectorAll('.paginated-carousel-item.original');
+  const paginationDots = block.parentElement.querySelectorAll('.pagination-dot');
 
+  const onScroll = (entries) => {
+    entries.forEach((entry) => {
+      const card = entry.target;
+      payload.currentIndex = entry.target.dataset.carouselIndex;
+
+      if (entry.isIntersecting) {
+        paginationDots.forEach((dot) => {
+          if (dot !== paginationDots[payload.currentIndex]) {
+            dot.classList.remove('active');
+          }
+        });
+        paginationDots[payload.currentIndex].classList.add('active');
+
+        if (card.classList.contains('prev')) {
+          block.scrollBy({ left: originalCards.length * card.offsetWidth });
+        }
+
+        if (card.classList.contains('next') && payload.currentIndex === (originalCards.length - 1).toString()) {
+          block.scrollBy({ left: -(originalCards.length * card.offsetWidth) });
+        }
+      } else if (card.classList.contains('next') && payload.currentIndex !== (originalCards.length - 1).toString()) {
+        block.scrollBy({ left: -(originalCards.length * card.offsetWidth) });
+      }
+    });
+  };
+
+  const options = {
+    root: block,
+    rootMargin: '0px',
+    threshold: 1.00,
+  };
+
+  const scrollObserver = new IntersectionObserver(onScroll, options);
+
+  payload.cItemsNext.forEach((card) => {
+    block.append(card);
+  });
+
+  payload.cItemsPrev.reverse().forEach((card) => {
+    block.prepend(card);
+  });
+
+  const allCards = block.querySelectorAll('.paginated-carousel-item');
+  allCards.forEach((card) => {
+    scrollObserver.observe(card);
+    if (card.classList.contains('original') && card.dataset.carouselIndex === '0') {
+      block.scrollBy({ left: originalCards.length * card.offsetWidth });
+    }
+  });
+
+  block.addEventListener('scroll', () => {
+    if (!payload.backwardInfinite) {
+      const prevCards = block.querySelectorAll('.paginated-carousel-item.prev');
+      prevCards.forEach((pCard) => {
+        pCard.classList.remove('hidden');
+      });
+      payload.backwardInfinite = true;
+    }
+  }, { passive: true });
 }
 
 export default function decorate(block) {
+  const payload = {
+    cItemsPrev: [],
+    cItemsNext: [],
+    backwardInfinite: false,
+    currentIndex: 0,
+  };
+
   const cItems = block.querySelectorAll(':scope > div');
 
   if (cItems.length > 0) {
@@ -47,22 +139,19 @@ export default function decorate(block) {
       decorateParagraphs(ci);
     });
 
-    const cItemsPrev = Array.from(cItems).map((item, index) => {
-      const clone = item.cloneNode(true);
-      clone.className = 'paginated-carousel-item prev';
-      ci.dataset.carouselIndex = i;
-      decorateParagraphs(item);
-      return clone;
-    });
+    for (const [key] of Object.entries(payload)) {
+      if (['cItemsPrev', 'cItemsNext'].includes(key)) {
+        payload[key] = Array.from(cItems).map((item, index) => {
+          const clone = item.cloneNode(true);
+          clone.className = key === 'cItemsPrev' ? 'paginated-carousel-item prev hidden' : 'paginated-carousel-item next';
+          clone.dataset.carouselIndex = index.toString();
+          decorateParagraphs(item);
+          return clone;
+        });
+      }
+    }
 
-    const cItemsNext = Array.from(cItems).map((item) => {
-      const clone = item.cloneNode(true);
-      clone.className = 'paginated-carousel-item next';
-      decorateParagraphs(item);
-      return clone;
-    });
-
-    decoratePagination(block);
-    initCarousel(block);
+    decoratePagination(block, payload);
+    initCarousel(block, payload);
   }
 }
