b536b74f0813f913d646157de7b640f51bf7db66
William Ste-Marie <williambsm@hotmail.fr>
2023-03-01 17:44:48 -0500

    feat(start-from-card): spreadsheet powered and refactor

--- express/blocks/start-from-card/start-from-card.js
+++ express/blocks/start-from-card/start-from-card.js
@@ -23,30 +23,40 @@ export default async function decorate($block) {
   const payload = {
     carouselArray: [],
     other: [],
+    relevantRowsData: null,
   };
-  const relevantRowsData = await fetchRelevantRows(window.location.pathname);
 
-  for (const $row of $block.children) {
-    const $divs = $row.querySelectorAll('div');
-    switch ($divs[0].textContent.trim()) {
-      default:
-        payload.other.push($divs);
-        break;
-      case 'Heading':
-        if (relevantRowsData) {
-          const heading = createTag('h3');
-          heading.innerText = relevantRowsData.header;
-          $row.replaceWith(heading);
-        }
-        break;
-      case 'Feature Carousel':
-        buildPaginatedCarousel(':scope > div > p', $row, true);
-        break;
-      case 'App Store Badge':
-        $row.replaceWith(buildAppStoreBadge($divs[1].firstElementChild.href, { class: 'gradient-border' }));
-        break;
+  if ($block.classList.contains('spreadsheet-powered')) {
+    payload.relevantRowsData = await fetchRelevantRows(window.location.pathname);
+
+    if (payload.relevantRowsData && payload.relevantRowsData.startFromCard !== 'Y') {
+      $block.remove();
     }
   }
+
+  Array.from($block.children).forEach(($row) => {
+    const $columns = Array.from($row.children);
+    const parameter = $columns[0].textContent.trim();
+    const $value = $columns[1];
+
+    if (parameter === 'Heading') {
+      const $heading = createTag('h3');
+      let headingText = $value.textContent;
+
+      if (payload.relevantRowsData && payload.relevantRowsData.startFromCardTitle) {
+        headingText = payload.relevantRowsData.startFromCardTitle;
+      }
+
+      $heading.textContent = headingText;
+      $row.replaceWith($heading);
+    } else if (parameter === 'Feature Carousel') {
+      buildPaginatedCarousel(':scope > div > p', $row, true);
+    } else if (parameter === 'App Store Badge') {
+      $row.replaceWith(buildAppStoreBadge($value.firstElementChild.href, { class: 'gradient-border' }));
+    } else {
+      payload.other.push($columns);
+    }
+  });
 
   const freePlanTags = await buildStaticFreePlanWidget();
   $block.insertAdjacentElement('afterend', freePlanTags);
