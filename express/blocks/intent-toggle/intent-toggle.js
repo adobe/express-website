f7b2c582a5c0f041e097cb1f50ad0c3946b7ac33
Qiyun Dai <pianoquin@gmail.com>
2023-03-07 20:54:51 -0600

    feat(intent-toggle): prototype done

--- express/blocks/intent-toggle/intent-toggle.js
+++ express/blocks/intent-toggle/intent-toggle.js
@@ -63,7 +63,53 @@ function initButton($block, $sections, index) {
   }
 }
 
-function decorateButtons($block, container) {
+function motionReduced() {
+  return window.matchMedia('(prefers-reduced-motion: reduce)') === true
+    || window.matchMedia('(prefers-reduced-motion: reduce)').matches === true;
+}
+
+function buildReduceMotionSwitch($block, container) {
+  const reduceMotionSwitch = createTag('div', { class: 'reduce-motion-switch' });
+  const reduceMotionSlider = createTag('div', { class: 'reduce-motion-slider' });
+  const reduceMotionKnob = createTag('div', { class: 'reduce-motion-knob' });
+  const reduceMotionText = createTag('span', { class: 'reduce-motion-text' });
+
+  reduceMotionText.textContent = container.textContent.trim();
+  container.innerHTML = '';
+
+  reduceMotionSwitch.append(reduceMotionSlider);
+  reduceMotionSlider.append(reduceMotionKnob);
+  container.prepend(reduceMotionSwitch, reduceMotionText);
+  $block.prepend(container);
+
+  if (sessionStorage.getItem('reduceMotion') === null) {
+    if (motionReduced()) {
+      sessionStorage.setItem('reduceMotion', 'on');
+      reduceMotionSlider.classList.add('on');
+    } else {
+      sessionStorage.setItem('reduceMotion', 'off');
+    }
+  } else if (sessionStorage.getItem('reduceMotion') === 'on') reduceMotionSlider.classList.add('on');
+
+  reduceMotionSwitch.addEventListener('click', () => {
+    reduceMotionSlider.classList.toggle('on');
+    if (sessionStorage.getItem('reduceMotion') === 'on') {
+      sessionStorage.setItem('reduceMotion', 'off');
+    } else {
+      sessionStorage.setItem('reduceMotion', 'on');
+    }
+
+    const reduceMotionToggled = new CustomEvent('reducemotiontoggled', {
+      detail: { state: sessionStorage.getItem('reduceMotion') },
+    });
+
+    document.dispatchEvent(reduceMotionToggled);
+  });
+
+  container.classList.add('reduce-motion-switch-container');
+}
+
+function decorateToggleButtons($block, container) {
   const $enclosingMain = $block.closest('main');
   if ($enclosingMain) {
     const $sections = $enclosingMain.querySelectorAll('[data-toggle]');
@@ -78,7 +124,7 @@ function decorateButtons($block, container) {
 
       content.forEach(($toggle, index) => {
         const $button = createTag('button', { class: 'intent-toggle-button' });
-        const tagText = $toggle.textContent.trim().match(/\[(.*?)\]/);
+        const tagText = $toggle.textContent.trim().match(/\[(.*?)]/);
 
         if (tagText) {
           const [fullText, tagTextContent] = tagText;
@@ -99,6 +145,22 @@ function decorateButtons($block, container) {
   }
 }
 
+function buildQuickActions($block, container) {
+  const paragraphs = container.querySelectorAll('p');
+  paragraphs.forEach((p) => {
+    const img = p.querySelector('img');
+    const a = p.querySelector('a');
+
+    if (img && a) {
+      a.prepend(img);
+      container.append(a);
+      p.remove();
+    }
+  });
+  $block.append(container);
+  container.classList.add('quick-action-buttons');
+}
+
 export default function decorate($block) {
   const $enclosingMain = $block.closest('main');
   if ($enclosingMain) {
@@ -113,15 +175,15 @@ export default function decorate($block) {
         const contentContainer = columns[1];
 
         if (parameter === 'accessibility') {
-          // todo: build reduce motion toggle button
+          buildReduceMotionSwitch($block, contentContainer);
         }
 
         if (parameter === 'toggle') {
-          decorateButtons($block, contentContainer);
+          decorateToggleButtons($block, contentContainer);
         }
 
         if (parameter === 'quick actions') {
-          // todo: build two quick action buttons
+          buildQuickActions($block, contentContainer);
         }
       }
     });
