2923e399ea8bdecb4e10db52ff6ec3e89ebc24d9
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-03-17 11:58:06 -0600

    style (breadcrumbs): spacing missing
    Co-authored-by: Raphael Wegmueller <github@rofe.com>

--- express/scripts/gnav.js
+++ express/scripts/gnav.js
@@ -147,7 +147,7 @@ function loadFEDS() {
 
   const isHomepage = window.location.pathname.endsWith('/express/');
   const isMegaNav = window.location.pathname.startsWith('/express')
-  || window.location.pathname.startsWith('/education');
+    || window.location.pathname.startsWith('/education');
   const fedsExp = isMegaNav
     ? `adobe-express/ax-gnav${isHomepage ? '-homepage' : ''}`
     : 'cc-express/cc-express-gnav';
