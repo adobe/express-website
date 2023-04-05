8954151e9729145c0666d482effbd2318ca6d5fc
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-04-05 09:33:30 -0600

    refactor(circle-list-desktop): Cleaned up code

--- express/blocks/circle-list-desktop/circle-list-desktop.js
+++ express/blocks/circle-list-desktop/circle-list-desktop.js
@@ -14,11 +14,11 @@ import { createTag, getLottie, lazyLoadLottiePlayer } from '../../scripts/script
 import preferenceStore, { eventNames } from '../../scripts/preference-store.js';
 
 // Fetches images from the spreadsheet
-async function fetchCircleImages(link) {
+const fetchCircleImages = async (link) => {
   const resp = await fetch(`${link}`);
   const json = await resp.json();
   return json.data;
-}
+};
 
 // Fetches the corresponding row in the spreadsheet based on the label name
 const grabImageSet = (category, imageData) => {
@@ -26,22 +26,7 @@ const grabImageSet = (category, imageData) => {
   return Object.values(targetRow).slice(1);
 };
 
-const buildDropdownList = (circle) => {
-  const list = createTag('ul', { class: 'dropdown' });
-  list.classList.add('hidden', 'transparent');
-  circle.dropDownOptions.forEach((option) => {
-    const li = createTag('li');
-    const aTag = createTag('a');
-    const label = createTag('span');
-
-    label.textContent = option.text;
-    aTag.href = option.link;
-    aTag.append(option.icon, label);
-    li.append(aTag);
-    list.append(li);
-  });
-  return list;
-};
+const lowerHyphenate = (word) => word.toLowerCase().replaceAll(' ', '-');
 
 const extractContent = async (block) => {
   const title = block.firstElementChild.querySelector('h1');
@@ -81,7 +66,7 @@ const extractContent = async (block) => {
       circleObject.dropDownOptions = dropDownOptions;
       circleObject.label = label;
     } else if (colArray.length === 2) {
-      circleObject.lottie = getLottie('blank-canvas', '/express/blocks/circle-list-desktop/blank-canvas.json');
+      circleObject.lottie = getLottie(lowerHyphenate(label), `/express/blocks/circle-list-desktop/${lowerHyphenate(label)}.json`);
       circleObject.subText = label;
       circleObject.type = 'lottie';
       circleObject.label = colArray[1].textContent.trim();
@@ -89,35 +74,52 @@ const extractContent = async (block) => {
     circleObject.defaultLink = defaultLink;
     circleList.push(circleObject);
   });
+
   block.innerHTML = '';
   block.append(title, subTitle);
   return circleList;
 };
 
+const buildDropdownList = (circle) => {
+  const list = createTag('ul', { class: 'dropdown' });
+  list.classList.add('hidden', 'transparent');
+  circle.dropDownOptions.forEach((option) => {
+    const li = createTag('li');
+    const aTag = createTag('a');
+    const label = createTag('span');
+
+    label.textContent = option.text;
+    aTag.href = option.link;
+    aTag.append(option.icon, label);
+    li.append(aTag);
+    list.append(li);
+  });
+  return list;
+};
+
 const buildCircleList = (block, circles) => {
   const circleContainer = createTag('div', { class: 'circles-container' });
+
   circles.forEach((circle) => {
     const circleWrapper = createTag('div', { class: 'circle-wrapper' });
     const defaultLink = createTag('a');
-
     defaultLink.href = circle.defaultLink;
+
     if (circle.type !== 'lottie') {
       const imageWrapper = createTag('div', { class: 'img-wrapper' });
       const heroImage = createTag('img', { class: 'hero-img' });
       [heroImage.src] = circle.imageLinks;
       imageWrapper.append(heroImage);
-
       for (let i = 1; i < circle.imageLinks.length; i += 1) {
         const altImg = createTag('img', { class: 'alt-img' });
         altImg.src = circle.imageLinks[i];
         imageWrapper.append(altImg);
       }
-
       circleWrapper.append(imageWrapper);
       circleWrapper.append(buildDropdownList(circle));
     } else {
-      const lottieWrapper = createTag('div', { class: 'lottie-wrapper' });
       lazyLoadLottiePlayer();
+      const lottieWrapper = createTag('div', { class: 'lottie-wrapper' });
       const lottie = createTag('div', { class: 'lottie-animation' });
       const subText = createTag('span', { class: 'sub-text' });
       subText.textContent = circle.subText;
@@ -136,20 +138,20 @@ const buildCircleList = (block, circles) => {
   block.append(circleContainer);
 };
 
-// Adds a class to handle transitions for door handle appearing
-function initDoorHandle(imgWrapper, dropDown, circleWrapper) {
+// Handles appearance of door handle
+const initDoorHandle = (imgWrapper, dropDown, circleWrapper) => {
   circleWrapper.addEventListener('mouseenter', () => {
     dropDown.classList.remove('hidden');
     setTimeout(() => {
       dropDown.classList.remove('transparent');
     }, 10);
   });
-}
+};
 
 // Resets all the images to their default state and handles classes transition:off classes
-function initResetDoorHandle(wrapper, dropDown) {
+const initResetDoorHandle = (wrapper, dropDown) => {
   wrapper.addEventListener('mouseleave', (e) => {
-    const hoveredImgs = Array.from(e.currentTarget.querySelectorAll('img'));
+    const images = Array.from(e.currentTarget.querySelectorAll('img'));
     const circleWrapper = wrapper.querySelector('.circle-wrapper');
 
     circleWrapper.setAttribute('style', 'z-index: 1');
@@ -157,76 +159,53 @@ function initResetDoorHandle(wrapper, dropDown) {
     setTimeout(() => {
       dropDown.classList.add('hidden');
     }, 201);
-    hoveredImgs.forEach((img) => {
+    images.forEach((img) => {
       img.setAttribute('style', 'transform-style: preserve-3d; z-index: 0');
     });
   });
-}
+};
 
-let oldMouseX = null;
-function detectMouseDirection(e) {
-  let direction = null;
-  if (oldMouseX !== null) {
-    const newMouseX = e.clientX;
-    if (newMouseX > oldMouseX) {
-      direction = 'right';
-    } else if (newMouseX < oldMouseX) {
-      direction = 'left';
-    }
-  }
-  oldMouseX = e.clientX;
-  return direction;
-}
-
-// Snaps back to hero image, even if mouse leaves image on partial transition
-function initResetHeroImage(imgWrapper) {
+// Snaps back to hero image, when mouse leaves photo
+const initResetHeroImage = (imgWrapper) => {
   const heroImage = imgWrapper.querySelector('.hero-img');
   const altImages = Array.from(imgWrapper.querySelectorAll('.alt-img'));
 
   imgWrapper.addEventListener('mouseleave', () => {
-    heroImage.setAttribute('style', 'transform: scale3d(1, 1, 1); transform-style: preserve-3d; opacity: 1');
+    heroImage.setAttribute('style', 'transform: scale3d(1, 1, 1); opacity: 1');
     altImages.forEach((altImg) => {
       altImg.setAttribute('style', 'opacity: 0');
     });
   });
-}
+};
 
-function initImageShuffling(wrapper, block) {
-  const imageWrapper = wrapper.querySelector('.img-wrapper');
+// Handles the shuffling of images based on mouse position
+const initImageShuffling = (wrapper, imageWrapper, block) => {
   const imageCount = imageWrapper.querySelectorAll('img').length;
-  let activeImageIndex = 0;
-  let backgroundImageIndex = 0;
-  const player = block.querySelector('lottie-player');
+  const lottiePlayer = block.querySelector('lottie-player');
   const reducedMotion = preferenceStore.get(eventNames.reduceMotion);
   const circleWrapper = wrapper.querySelector('.circle-wrapper');
+  let activeImageIndex = 0;
+  let backgroundImageIndex = 0;
 
-  function chooseImage(e) {
-    const wrapperWidth = imageWrapper.offsetWidth;
-    const switchPxThreshold = wrapperWidth / imageCount;
-    const photoList = Array.from(imageWrapper.children);
-    const mouseX = e.clientX - wrapper.offsetLeft < 0 ? 0 : e.clientX - wrapper.offsetLeft;
-
-    backgroundImageIndex = Math.max(activeImageIndex - 1, 0);
-    photoList[activeImageIndex].setAttribute('style', 'transform: scale3d(0.85, 0.85, 0.85); opacity: 0; z-index: 0');
-    photoList[backgroundImageIndex].setAttribute('style', 'transform: scale3d(0.85, 0.85, 0.85); opacity: 0');
-    activeImageIndex = Math.floor(mouseX / switchPxThreshold) >= imageCount ? imageCount - 1
-      : Math.floor(mouseX / switchPxThreshold);
-    backgroundImageIndex = Math.max(activeImageIndex - 1, 0);
-    const pxFromImageSwap = mouseX - activeImageIndex * switchPxThreshold;
-    const minResizeScale = 0.85;
-    const resizeScale = Math.max((100 + pxFromImageSwap - switchPxThreshold) / 100, minResizeScale);
-
-    photoList[activeImageIndex].setAttribute('style', `transform: scale3d(${resizeScale}, ${resizeScale}, ${resizeScale}); opacity: 1; z-index: 2; transition-property: transform`);
-    photoList[backgroundImageIndex].setAttribute('style', 'transform: scale3d(1, 1, 1); opacity: 1; z-index: 1; transition-property: none');
-  }
-
-  // Handles the shuffling of images based on mouse position
   const shuffle = (e) => {
     circleWrapper.setAttribute('style', 'z-index: 3');
     if (!block.classList.contains('no-animation')) {
-      setTimeout(() => {
-        chooseImage(e);
-      }, 200);
+      const wrapperWidth = imageWrapper.offsetWidth;
+      const switchPxThreshold = wrapperWidth / imageCount;
+      const photoList = Array.from(imageWrapper.children);
+      const mouseX = e.clientX - wrapper.offsetLeft < 0 ? 0 : e.clientX - wrapper.offsetLeft;
+      backgroundImageIndex = Math.max(activeImageIndex - 1, 0);
+      photoList[activeImageIndex].setAttribute('style', 'opacity: 0');
+      photoList[backgroundImageIndex].setAttribute('style', 'opacity: 0');
+      activeImageIndex = Math.floor(mouseX / switchPxThreshold) >= imageCount ? imageCount - 1
+        : Math.floor(mouseX / switchPxThreshold);
+      backgroundImageIndex = Math.max(activeImageIndex - 1, 0);
+      const pxFromImageSwap = mouseX - activeImageIndex * switchPxThreshold;
+      const minResizeScale = 0.85;
+      const reScale = Math.max((100 + pxFromImageSwap - switchPxThreshold) / 100, minResizeScale);
+
+      photoList[activeImageIndex].setAttribute('style', `transform: scale3d(${reScale}, ${reScale}, ${reScale}); opacity: 1; z-index: 2;`);
+      photoList[backgroundImageIndex].setAttribute('style', 'transform: scale3d(1, 1, 1); opacity: 1; z-index: 1;');
     } else {
       wrapper.querySelector('.hero-img').setAttribute('style', 'transform: scale3d(1, 1, 1); opacity: 1; z-index: 1');
     }
@@ -235,14 +214,14 @@ function initImageShuffling(wrapper, block) {
   // Pauses lottie and disables shuffling of images
   const toggleAnimationState = (reduceMotion) => {
     const lottiePlaying = setInterval(() => {
-      if (player.hasUpdated) {
+      if (lottiePlayer.hasUpdated) {
         if (reduceMotion === true) {
           block.classList.add('no-animation');
-          player.setSpeed(0);
-          player.seek(200);
+          lottiePlayer.setSpeed(0);
+          lottiePlayer.seek(200);
         } else {
           block.classList.remove('no-animation');
-          player.setSpeed(1);
+          lottiePlayer.setSpeed(1);
         }
         clearInterval(lottiePlaying);
       }
@@ -252,22 +231,21 @@ function initImageShuffling(wrapper, block) {
   toggleAnimationState(reducedMotion);
   preferenceStore.subscribe(eventNames.reduceMotion, block, (value) => toggleAnimationState(value));
   imageWrapper.addEventListener('mousemove', shuffle);
-}
+};
 
-export default async function decorate($block) {
-  const circleList = await extractContent($block);
-  buildCircleList($block, circleList);
-  const circleWrappers = $block.querySelectorAll('.circles-container > a');
+export default async function decorate(block) {
+  const circleList = await extractContent(block);
+  buildCircleList(block, circleList);
+  const circleWrappers = block.querySelectorAll('.circles-container > a');
 
   circleWrappers.forEach((wrapper) => {
     const imageWrapper = wrapper.querySelector('.img-wrapper');
     const dropDown = wrapper.querySelector('.dropdown');
     if (imageWrapper) {
       initDoorHandle(imageWrapper, dropDown, wrapper);
-      initResetDoorHandle(wrapper, dropDown);
-      initImageShuffling(wrapper, $block);
+      initImageShuffling(wrapper, imageWrapper, block);
       initResetHeroImage(imageWrapper);
+      initResetDoorHandle(wrapper, dropDown);
     }
   });
 }
-
