d06cd6026d5ab9166b7dafd2922764f68a287d8e
Ravi Kiran Papani <rpapani@adobe.com>
2023-04-18 18:19:33 -0700

    SITES-12878 - Frictionless quickaction phase2

--- express/blocks/quick-action/shared.js
+++ express/blocks/quick-action/shared.js
@@ -148,11 +148,10 @@ export class CCXQuickActionElement extends HTMLElement {
 
   // eslint-disable-next-line class-methods-use-this
   handleNavigateToPostEditor(data, autoDownload = false) {
-    const host = 'https://express.adobe.com';
+    const host = 'https://project-marvel-theo-web-8569.fracture.adobeprojectm.com/';
     const action = 'remove-background';
     const { repositoryId, transientToken } = data;
-    const path = '/sp/design/post/new';
-    const loginUrl = '/sp/login?destination=';
+    const path = '/design/post/new';
     const params = new URLSearchParams();
     params.append('workflow', 'quicktask');
     params.append('r', 'qtImaging');
@@ -163,10 +162,8 @@ export class CCXQuickActionElement extends HTMLElement {
     if (transientToken) {
       params.append('transientToken', transientToken);
     }
-    const encodeUrl = encodeURIComponent(`${host}${path}?${params.toString()}`);
-    const url = `${host}${loginUrl}${encodeUrl}`;
-    // FIXME: verify if this is the right URL
-    window.location.replace(url);
+    const encodeUrl = `$${host}/sp${path}?${params.toString()}`;
+    window.location.replace(encodeUrl);
   }
 
   async render() {
