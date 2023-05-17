dea2eccac9f139cc0f37d3f515c1394537858402
Qiyun Dai <pianoquin@gmail.com>
2023-05-17 11:44:29 -0500

    reverting api key

--- express/blocks/search-marquee/autocomplete-api-v3.js
+++ express/blocks/search-marquee/autocomplete-api-v3.js
@@ -25,7 +25,7 @@ export default async function fetchAPI({ limit = 5, textQuery, locale = 'en-US' 
   const res = await fetch(url, {
     method: 'POST',
     headers: {
-      'x-api-key': 'projectx_marketing_web',
+      'x-api-key': 'projectx_webapp',
       'content-type': 'application/json',
     },
     body: JSON.stringify({
