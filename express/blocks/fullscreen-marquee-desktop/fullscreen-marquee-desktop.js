d601864e61fb1743aa5c79688cc24a1cb3dd20a5
Qiyun Dai <pianoquin@gmail.com>
2023-06-15 14:48:20 -0500

    Update fullscreen-marquee-desktop.js

--- express/blocks/fullscreen-marquee-desktop/fullscreen-marquee-desktop.js
+++ express/blocks/fullscreen-marquee-desktop/fullscreen-marquee-desktop.js
@@ -96,7 +96,7 @@ async function buildApp(block, content) {
 
         createFrame(1, 10);
         app.append(framesContainer);
-        console.log(Math.floor(thumbnail.duration))
+        console.log(thumbnail.duration);
         thumbnail.currentTime = Math.floor(thumbnail.duration);
         thumbnail.pause();
       });
