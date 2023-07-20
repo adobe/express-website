88e8cf86bef35eef159f9d9965a4998a4af5e955
Qiyun Dai <pianoquin@gmail.com>
2023-07-20 12:59:52 -0500

    reenable cross toolbar filter sync

--- express/blocks/template-x/template-x.js
+++ express/blocks/template-x/template-x.js
@@ -892,9 +892,7 @@ async function redrawTemplates(block, props, toolBar) {
   await decorateNewTemplates(block, props, { reDrawMasonry: true });
 
   heading.textContent = heading.textContent.replace(`${currentTotal}`, props.total.toLocaleString('en-US'));
-  // await updateOptionsStatus(block, props, toolBar);
-  /* fixme: toolbar now has issue syncing filter status with mobile view.
-  Temporarily disabling the feature as block will be authored for each audience separately. */
+  await updateOptionsStatus(block, props, toolBar);
   if (block.querySelectorAll('.template').length <= 0) {
     const $viewButtons = toolBar.querySelectorAll('.view-toggle-button');
     $viewButtons.forEach((button) => {
