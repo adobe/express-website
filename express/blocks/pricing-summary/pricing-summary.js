3516892990e91c5c586acec9040ef6060e10a080
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2023-07-24 16:22:36 -0400

    Pricing-summary

--- express/blocks/pricing-summary/pricing-summary.js
+++ express/blocks/pricing-summary/pricing-summary.js
@@ -135,12 +135,6 @@ export default function decorate(block) {
 
   pricingContainer.classList.add('pricing-container');
 
-  Array.from(block.children)?.forEach((child) => {
-    if (child.children[0]?.childNodes.length === 0) {
-      child.children[0]?.remove();
-    }
-  });
-
   const columnsContainer = createTag('div', { class: 'columns-container' });
 
   const columns = Array.from(pricingContainer.children);
--- test/blocks/pricing-summary/pricing-summary.test.js
+++ test/blocks/pricing-summary/pricing-summary.test.js
@@ -21,27 +21,50 @@ describe('Pricing Summary', () => {
   });
 
   it('Pricing Summary exists', () => {
-    const pricingSummary = document.body.querySelector('.pricing-summary');
+    const pricingSummary = document.querySelector('.pricing-summary');
     decorate(pricingSummary);
     expect(pricingSummary).to.exist;
   });
 
   it('Columns to be 2 or 3', () => {
-   const columns = document.body.querySelectorAll('.pricing-column');
+    const columns = document.querySelectorAll('.pricing-column');
+    expect(columns).to.exist;
     expect(columns.length).to.be.oneOf([2, 3]);
   });
 
-  it('Columns have correct elements', () => {
-    expect(document.body.querySelector('.pricing-header')).to.exist;
-    expect(document.body.querySelector('.pricing-description')).to.exist;
-    expect(document.body.querySelector('.pricing-plan')).to.exist;
-    expect(document.body.querySelector('.pricing-price')).to.exist;
+  it('Columns have correct elements', () =>{
+    expect(document.querySelector('.pricing-header')).to.exist;
+    expect(document.querySelector('.pricing-description')).to.exist;
+    expect(document.querySelector('.pricing-plan')).to.exist;
+    expect(document.querySelector('.pricing-price')).to.exist;
   });
 
+  it('Should have a free plan with correct details', () => {
+    const freePlan = document.querySelector('#free');
+    expect(freePlan).to.exist;
 
-  it('Should open link pop up when cta are clicked', () => {
-   
+    const freeIcon = document.querySelector('.icon-free-icon');
+    expect(freeIcon).to.exist;
+
+    const firstColumn = document.querySelectorAll('.pricing-column')[0];
+    const freeDescription = firstColumn.querySelector('.pricing-description');
+    expect(freeDescription.textContent).to.include('Free use');
+  });
+
+  it('Should have a premium plan with correct details', () => {
+    const premiumPlan = document.querySelector('#premium');
+    expect(premiumPlan).to.exist;
+
+    const premiumIcon = document.querySelector('.icon-premium-icon');
+    expect(premiumIcon).to.exist;
+
+    const secondColumn = document.querySelectorAll('.pricing-column')[1];
+    const premiumDescription = secondColumn.querySelector('.pricing-description');
+    expect(premiumDescription.textContent).to.include('premium features');
+  });
+
+  it('Should contain the correct billing notice', () =>{
+    const billingNotice = document.querySelector('.pricing-summary > div:last-child div');
+    expect(billingNotice.textContent.includes('Billing begins when your free trial ends')).to.be.true;
   });
 });
-
-
